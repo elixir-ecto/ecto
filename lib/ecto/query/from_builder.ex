90530c1cbb1956d516011284757b09bd065c56b1
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2013-06-26 15:26:10 +0200

    Add FromBuilder

---
+++ lib/ecto/query/from_builder.ex
@@ -0,0 +1,12 @@
+defmodule Ecto.Query.FromBuilder do
+  @moduledoc false
+
+  def escape({ :in, _, [{ var, _, context}, {:__aliases__, _, _} = record] })
+      when is_atom(var) and is_atom(context) do
+    { var, record }
+  end
+
+  def escape(_other) do
+    raise ArgumentError, message: "only `in` expressions binding variables to records allowed"
+  end
+end
---
+++ test/ecto/query/from_builder_test.exs
@@ -0,0 +1,35 @@
+Code.require_file "../../test_helper.exs", __DIR__
+
+defmodule Ecto.Query.FromBuilderTest do
+  use ExUnit.Case
+
+  import Ecto.Query.FromBuilder
+
+  test "escape" do
+    assert { :x, quote do X end } ==
+           escape(quote do x in X end)
+
+    assert { :x, quote do X.Y.Z end } ==
+           escape(quote do x in X.Y.Z end)
+  end
+
+  test "escape raise" do
+    message = "only `in` expressions binding variables to records allowed"
+
+    assert_raise ArgumentError, message, fn ->
+      escape(quote do 1 end)
+    end
+
+    assert_raise ArgumentError, message, fn ->
+      escape(quote do f() end)
+    end
+
+    assert_raise ArgumentError, message, fn ->
+      escape(quote do x end)
+    end
+
+    assert_raise ArgumentError, message, fn ->
+      escape(quote do x in y end)
+    end
+  end
+end
