99d609cc6e39d7bfa5e06c0680ca86647f7533ad
José Valim <jose.valim@plataformatec.com.br>
2013-10-28 10:16:54 +0100

    Add Ecto.Validator

---
+++ lib/ecto/validator.ex
@@ -0,0 +1,76 @@
+defmodule Ecto.Validator do
+  @moduledoc """
+  Validates a given record or dict given a set of predicates:
+
+      Ecto.Validator.record(user,
+        name: present() when on_create?(user),
+        age: present(message: "must be present"),
+        also: validate_other
+      )
+
+  Validations are passed as the second argument in the attribute-predicate
+  format. Each predicate can be filtered via the `when` operator. Note `when`
+  here is not limited only to guard expressions.
+
+  The predicates above are going to receive the attribute being validated
+  and its current value as argument. For example, the `present` predicate
+  above is going to be called as:
+
+      present(:name, user.name)
+      present(:age, user.age, message: "must be present")
+
+  The validator also handles a special key `:also`, which is used to pipe
+  to predicates without a particular attribute. Instead, such predicates
+  receive the record as argument. In this example, `validate_other` will
+  be invoked as:
+
+      validate_other(user)
+
+  Note all validators must return a keyword list, with the attribute error
+  as key and the validation message as value.
+  """
+
+  @doc """
+  Validates a given record given a set of predicates.
+  """
+  @spec record(Macro.t, Keyword.t) :: Macro.t
+  defmacro record(value, opts) when is_list(opts) do
+    process opts, value, fn var, attr ->
+      quote do: unquote(var).unquote(attr)
+    end
+  end
+
+  defp process([], _value, _getter), do: []
+  defp process(opts, value, getter) do
+    var = quote do: var
+
+    validations = Enum.reduce opts, [], fn i, acc ->
+      quote do: unquote(acc) ++ unquote(process_each(i, var, getter))
+    end
+
+    quote do
+      unquote(var) = unquote(value)
+      unquote(validations)
+    end
+  end
+
+  defp process_each({ :also, function }, var, _getter) do
+    handle_when function, fn call -> Macro.pipe(var, call) end
+  end
+
+  defp process_each({ attr, function }, var, getter) do
+    handle_when function, fn call ->
+      Macro.pipe(attr, Macro.pipe(getter.(var, attr), call))
+    end
+  end
+
+  defp handle_when({ :when, _, [left, right] }, callback) do
+    quote do
+      if unquote(right), do: unquote(callback.(left)), else: []
+    end
+  end
+
+  defp handle_when(other, callback) do
+    callback.(other)
+  end
+end
---
+++ test/ecto/validator_test.exs
@@ -0,0 +1,55 @@
+defmodule Ecto.ValidatorTest do
+  use ExUnit.Case, async: true
+
+  import Ecto.Validator
+  defrecord User, name: "jose", age: 27
+
+  test "record with no predicate" do
+    assert record(User.new, []) == []
+  end
+
+  test "record dispatches to a module predicate" do
+    assert record(User.new, name: present()) == []
+    assert record(User.new(name: nil), name: present()) == [name: "can't be blank"]
+  end
+
+  test "record dispatches to a local predicate" do
+    present = &present/2
+    assert record(User.new, name: present.()) == []
+    assert record(User.new(name: nil), name: present.()) == [name: "can't be blank"]
+  end
+
+  test "record handles conditionals" do
+    user = User.new(age: nil)
+    assert record(user, age: present() when user.name != "jose") == []
+
+    user = User.new(name: "eric", age: nil)
+    assert record(user, age: present() when user.name != "jose") == [age: "can't be blank"]
+  end
+
+  test "record keeps predicate arguments" do
+    assert record(User.new(name: nil),
+                   name: present(message: "must be present")) == [name: "must be present"]
+  end
+
+  test "record is evaluated just once" do
+    Process.put(:count, 0)
+    assert record((Process.put(:count, Process.get(:count) + 1); User[]),
+             name: present()) == []
+    assert Process.get(:count) == 1
+  end
+
+  test "record dispatches to also" do
+    assert record(User.new(name: nil, age: nil),
+             name: present(),
+             also: validate_other) == [name: "can't be blank", age: "can't be blank"]
+  end
+
+  defp present(attr, value, opts // [])
+  defp present(attr, nil, opts), do: [{ attr, opts[:message] || "can't be blank" }]
+  defp present(_attr, _value, _opts), do: []
+
+  defp validate_other(record) do
+    Ecto.Validator.record(record, age: present())
+  end
+end
