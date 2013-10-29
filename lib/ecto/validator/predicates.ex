011a0fde5479f664262950296c2115ff11877758
José Valim <jose.valim@plataformatec.com.br>
2013-10-29 16:29:37 +0100

    Add has_format and has_length predicates

--- lib/ecto/validator/predicates.ex
+++ lib/ecto/validator/predicates.ex
@@ -61,7 +61,7 @@ defmodule Ecto.Validator.Predicates do
   end
 
   @doc """
-  Validates the attribute matches a given regular expression.
+  Validates the attribute has a given format.
   Nil values are not matched against (skipped).
 
   ## Options
@@ -71,17 +71,92 @@ defmodule Ecto.Validator.Predicates do
   ## Examples
 
       validate user,
-        email: matches(%r/@/)
+        email: has_format(%r/@/)
 
   """
-  @spec matches(atom, term, Regex.t | binary, Keyword.t) :: maybe_error
-  def matches(attr, value, match_on, opts // []) when is_regex(match_on) and is_list(opts) do
+  @spec has_format(atom, term, Regex.t | binary, Keyword.t) :: maybe_error
+  def has_format(attr, value, match_on, opts // []) when is_regex(match_on) and is_list(opts) do
     if value == nil or Regex.match?(match_on, value) do
       []
     else
       [{ attr, opts[:message] || "is invalid" }]
     end
   end
+
+  @doc """
+  Validates the attribute has a given length according to Unicode
+  (i.e. it uses `String.length` under the scenes). That said, this
+  function should not be used to validate binary fields.
+
+  The length can be given as a range (indicating min and max),
+  as an integer (indicating exact match) or as keyword options,
+  indicating, min and max values.
+
+  Raises if the given argument is not a binary.
+
+  ## Options
+
+  * `:too_long`  - message when the length is too long
+                   (defaults to "is too long (maximum is X characters)")
+  * `:too_short` - message when the length is too short
+                   (defaults to "is too short (minimum is X characters)")
+  * `:no_match` - message when the length does not match
+                  (defaults to "must be X characters")
+
+  ## Examples
+
+      validate user,
+        password: has_length(6..100)
+
+      validate user,
+        password: has_length(min: 6, too_short: "requires a minimum length")
+
+      validate user,
+        code: has_length(3, no_match: "needs to be 3 characters")
+
+  """
+  @spec has_length(atom, term, Regex.t | binary, Keyword.t) :: maybe_error
+  def has_length(attr, value, match_on, opts // [])
+
+  def has_length(_attr, nil, _match_on, opts) when is_list(opts) do
+    []
+  end
+
+  def has_length(attr, value, min..max, opts) when is_binary(value) when is_list(opts) do
+    length = String.length(value)
+    too_short(attr, length, min, opts) || too_long(attr, length, max, opts) || []
+  end
+
+  def has_length(attr, value, exact, opts) when is_integer(exact) do
+    if String.length(value) != exact do
+      [{ attr, opts[:no_match] || "must be #{characters(exact)}" }]
+    else
+      []
+    end
+  end
+
+  def has_length(attr, value, opts, other) when is_list(opts) and is_list(other) do
+    opts   = Keyword.merge(opts, other)
+    length = String.length(value)
+    ((min = opts[:min]) && too_short(attr, length, min, opts)) ||
+    ((max = opts[:max]) && too_long(attr, length, max, opts)) ||
+    []
+  end
+
+  defp too_short(attr, length, min, opts) when is_integer(min) do
+    if length < min do
+      [{ attr, opts[:too_short] || "is too short (minimum is #{characters(min)})" }]
+    end
+  end
+
+  defp too_long(attr, length, max, opts) when is_integer(max) do
+    if length > max do
+      [{ attr, opts[:too_long] || "is too long (maximum is #{characters(max)})" }]
+    end
+  end
+
+  defp characters(1), do: "1 character"
+  defp characters(x), do: "#{x} characters"
 
   @doc """
   Validates the attribute is member of the given enumerable.
--- test/ecto/validator/predicates_test.exs
+++ test/ecto/validator/predicates_test.exs
@@ -34,22 +34,72 @@ defmodule Ecto.Validator.PredicatesTest do
     assert absent(:name, "ok", message: "are you human?") == [name: "are you human?"]
   end
 
-  ## Matches
+  ## Has format
 
-  test "matches on invalid" do
-    assert matches(:name, "hello world", %r"sample") == [name: "is invalid"]
+  test "has_format on invalid" do
+    assert has_format(:name, "hello world", %r"sample") == [name: "is invalid"]
   end
 
-  test "matches on valid" do
-    assert matches(:name, "hello", %r"hello") == []
+  test "has_format on valid" do
+    assert has_format(:name, "hello", %r"hello") == []
   end
 
-  test "matches skips on nil" do
-    assert matches(:name, nil, %r"hello") == []
+  test "has_format skips on nil" do
+    assert has_format(:name, nil, %r"hello") == []
   end
 
-  test "matches with custom message with custom message" do
-    assert matches(:name, "hello world", %r"sample", message: "no match") == [name: "no match"]
+  test "has_format with custom message" do
+    assert has_format(:name, "hello world", %r"sample", message: "no match") == [name: "no match"]
+  end
+
+  ## Has length
+
+  test "has_length with range on invalid" do
+    assert has_length(:name, "hello", 1..3) == [name: "is too long (maximum is 3 characters)"]
+    assert has_length(:name, "hello", 7..9) == [name: "is too short (minimum is 7 characters)"]
+  end
+
+  test "has_length with range on valid" do
+    assert has_length(:name, "hello", 3..7) == []
+  end
+
+  test "has_length with range with custom message" do
+    assert has_length(:name, "hello", 1..3, too_long: "is too long") == [name: "is too long"]
+    assert has_length(:name, "hello", 7..9, too_short: "is too short") == [name: "is too short"]
+  end
+
+  test "has_length with opts on invalid" do
+    assert has_length(:name, "hello", min: 1, max: 3) == [name: "is too long (maximum is 3 characters)"]
+    assert has_length(:name, "hello", min: 7, max: 9) == [name: "is too short (minimum is 7 characters)"]
+  end
+
+  test "has_length with opts on valid" do
+    assert has_length(:name, "hello", min: 3, max: 7) == []
+  end
+
+  test "has_length with opts with custom message" do
+    assert has_length(:name, "hello", [min: 1, max: 3], too_long: "is too long") == [name: "is too long"]
+    assert has_length(:name, "hello", min: 7, max: 9, too_short: "is too short") == [name: "is too short"]
+  end
+
+  test "has_length with integer on invalid" do
+    assert has_length(:name, "hello", 3) == [name: "must be 3 characters"]
+  end
+
+  test "has_length with integer on valid" do
+    assert has_length(:name, "hello", 5) == []
+  end
+
+  test "has_length with integer with custom message" do
+    assert has_length(:name, "hello", 3, no_match: "is wrong") == [name: "is wrong"]
+  end
+
+  test "has_length handles pluralization" do
+    assert has_length(:name, "hello", 1) == [name: "must be 1 character"]
+  end
+
+  test "has_length skips on nil" do
+    assert has_length(:name, nil, 3..5) == []
   end
 
   ## Member of
@@ -68,7 +118,7 @@ defmodule Ecto.Validator.PredicatesTest do
     assert member_of(:name, nil, %w(foo bar baz)) == []
   end
 
-  test "member_of with custom message with custom message" do
+  test "member_of with custom message" do
     assert member_of(:name, 7, 1..5, message: "not a member") == [name: "not a member"]
   end
 
@@ -88,7 +138,7 @@ defmodule Ecto.Validator.PredicatesTest do
     assert not_member_of(:name, nil, %w(foo bar baz)) == []
   end
 
-  test "not_member_of with custom message with custom message" do
+  test "not_member_of with custom message" do
     assert not_member_of(:name, 3, 1..5, message: "is taken") == [name: "is taken"]
   end
 end
