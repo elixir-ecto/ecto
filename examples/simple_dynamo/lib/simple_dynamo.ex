9e8e56089f9ae694c682f6a88b5aac32dbea954b
Johnny Winn <dev+nurugger07@hashrocket.com>
2013-11-12 21:33:42 -0500

    Add Ecto modules to display example query results

--- examples/simple_dynamo/lib/simple_dynamo.ex
+++ examples/simple_dynamo/lib/simple_dynamo.ex
@@ -6,6 +6,52 @@ defmodule SimpleDynamo do
   application and its Dynamos.
   """
   def start(_type, _args) do
-    SimpleDynamo.Dynamo.start_link([max_restarts: 5, max_seconds: 5])
+    SimpleDynamo.Sup.start_link
+  end
+end
+
+defmodule SimpleDynamo.Sup do
+  use Supervisor.Behaviour
+
+  def start_link do
+    :supervisor.start_link({ :local, __MODULE__ }, __MODULE__, [])
+  end
+
+  def init([]) do
+    tree = [ worker(SimpleDynamo.Dynamo, []), worker(Repo, []) ]
+    supervise(tree, strategy: :one_for_all)
+  end
+end
+
+defmodule Repo do
+  use Ecto.Repo, adapter: Ecto.Adapters.Postgres
+
+  # url ecto://username:password@hostname/database
+  def url, do: "ecto://johnny@localhost/simple_dynamo_db"
+end
+
+defmodule Weather do
+  use Ecto.Model
+
+  queryable "weather" do
+    field :city, :string
+    field :temp_lo, :integer
+    field :temp_hi, :integer
+    field :prcp, :float, default: 0.0
+  end
+end
+
+defmodule WeatherQueries do
+  import Ecto.Query
+
+  def all do
+    Enum.map(_all, fn(x) -> {x.id, x.city, x.temp_lo, x.temp_hi, x.prcp} end)
+  end
+
+  defp _all do
+    query = from w in Weather,
+            where: w.prcp > 0 or w.prcp == nil,
+            select: w
+    Repo.all(query)
   end
 end
--- examples/simple_dynamo/web/routers/application_router.ex
+++ examples/simple_dynamo/web/routers/application_router.ex
@@ -2,18 +2,11 @@ defmodule ApplicationRouter do
   use Dynamo.Router
 
   prepare do
-    # Pick which parts of the request you want to fetch
-    # You can comment the line below if you don't need
-    # any of them or move them to a forwarded router
     conn.fetch([:cookies, :params])
   end
 
-  # It is common to break your Dynamo into many
-  # routers, forwarding the requests between them:
-  # forward "/posts", to: PostsRouter
-
   get "/" do
-    conn = conn.assign(:title, "Welcome to Dynamo!")
-    render conn, "index.html"
+    conn = conn.assign(:title, "Average Weather by City")
+    render conn, "index.html", results: WeatherQueries.all
   end
 end
