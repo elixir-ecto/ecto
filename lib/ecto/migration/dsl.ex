252b68da834a91fbb9db9ff547cf5295a1c94a8a
Josh Nussbaum <joshnuss@gmail.com>
2013-11-23 05:27:27 -0500

    Refactored tables.create and tables.drop to create_table and drop_table

--- lib/ecto/migration/dsl.ex
+++ lib/ecto/migration/dsl.ex
@@ -2,19 +2,13 @@ defmodule Ecto.Migration.Dsl do
   defrecord CreateTable, name: nil
   defrecord DropTable, name: nil
 
-  defmodule Tables do
-    def create(name, fun) do
-      table = CreateTable.new(name: name)
-      fun.(table)
-      table
-    end
-
-    def drop(name) do
-      DropTable.new(name: name)
-    end
+  def drop_table(name) do
+    DropTable.new(name: name)
   end
 
-  def tables do
-    Tables
+  def create_table(name, fun) do
+    table = CreateTable.new(name: name)
+    fun.(table)
+    table
   end
 end
--- test/ecto/migration/dsl_test.exs
+++ test/ecto/migration/dsl_test.exs
@@ -3,14 +3,14 @@ defmodule Ecto.MigratorDslTest do
   import Ecto.Migration.Dsl
 
   test "creating table" do
-    ast = tables.create(:products, fn(_) -> nil end)
+    command = create_table(:products, fn(_) -> nil end)
 
-    assert ast == Ecto.Migration.Dsl.CreateTable[name: :products]
+    assert command == Ecto.Migration.Dsl.CreateTable[name: :products]
   end
 
   test "dropping table" do
-    ast = tables.drop(:products)
+    command = drop_table(:products)
 
-    assert ast == Ecto.Migration.Dsl.DropTable[name: :products]
+    assert command == Ecto.Migration.Dsl.DropTable[name: :products]
   end
 end
