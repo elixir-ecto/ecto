788d0095f3816546937910ad855b4d58686cf756
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2013-12-02 23:32:13 +0100

    Add assoc/2 select builder test

--- test/ecto/query/select_builder_test.exs
+++ test/ecto/query/select_builder_test.exs
@@ -26,4 +26,16 @@ defmodule Ecto.Query.SelectBuilderTest do
     assert (quote do x.y end) ==
             escape(quote do ^x.y end, [])
   end
+
+  test "escape assoc" do
+    assert Macro.escape(quote do assoc(&0, comments: &1) end) ==
+            escape(quote do assoc(p, comments: c) end, [:p, :c])
+
+    assert Macro.escape(quote do assoc(&0, comments: assoc(&1, author: &2), author: &3) end) ==
+            escape(quote do assoc(p, comments: assoc(c, author: cu), author: pu) end, [:p, :c, :cu, :pu])
+
+    assert_raise Ecto.QueryError, fn ->
+      escape(quote do assoc(var, :hey) end, [:var])
+    end
+  end
 end
