25725db00422c44df85e7f6839171ff6452e651a
Josh Nussbaum <joshnuss@gmail.com>
2013-12-13 21:52:32 -0500

    Added aliases to Migration.Dsl tests

--- test/ecto/migration/dsl_test.exs
+++ test/ecto/migration/dsl_test.exs
@@ -1,61 +1,63 @@
 defmodule Ecto.Migration.DslTest do
   use ExUnit.Case
+
+  alias Ecto.Migration.Dsl.CreateTable
+  alias Ecto.Migration.Dsl.ChangeTable
+  alias Ecto.Migration.Dsl.DropTable
+  alias Ecto.Migration.Dsl.CreateIndex
+  alias Ecto.Migration.Dsl.DropIndex
+  alias Ecto.Migration.Dsl.Column
+
   import Ecto.Migration.Dsl
 
   test "creating table" do
     command = create_table(:products)
 
-    assert command == Ecto.Migration.Dsl.CreateTable[name: :products]
+    assert command == CreateTable[name: :products]
   end
 
   test "dropping table" do
     command = drop_table(:products)
 
-    assert command == Ecto.Migration.Dsl.DropTable[name: :products]
+    assert command == DropTable[name: :products]
   end
 
   test "creating index" do
     command = create_index(:products, [:name], unique: true)
 
-    assert command == Ecto.Migration.Dsl.CreateIndex[table_name: :products,
-                                                     columns: [:name],
-                                                     unique: true]
+    assert command == CreateIndex[table_name: :products, columns: [:name], unique: true]
   end
 
   test "dropping index" do
     command = drop_index(:products, [:name])
 
-    assert command == Ecto.Migration.Dsl.DropIndex[table_name: :products, columns: [:name]]
+    assert command == DropIndex[table_name: :products, columns: [:name]]
   end
 
   test "change table" do
     command = change_table(:products)
 
-    assert command == Ecto.Migration.Dsl.ChangeTable[name: :products]
+    assert command == ChangeTable[name: :products]
   end
 
   test "adding column" do
     command = add_column(:products, :summary, :string, limit: 20)
 
-    assert command == Ecto.Migration.Dsl.ChangeTable[name: :products,
-                                                     changes: [{:add, Ecto.Migration.Dsl.Column[name: :summary,
-                                                                                                type: :string,
-                                                                                                limit: 20]}]]
+    assert command == ChangeTable[name: :products, changes: [
+        {:add, Column[name: :summary, type: :string, limit: 20]}]]
   end
 
   test "remove column" do
     command = remove_column(:products, :summary)
 
-    assert command == Ecto.Migration.Dsl.ChangeTable[name: :products,
-                                                     changes: [{:remove, Ecto.Migration.Dsl.Column[name: :summary]}]]
+    assert command == ChangeTable[name: :products, changes: [
+        {:remove, Column[name: :summary]}]]
   end
 
   test "changing column" do
     command = change_column(:products, :summary, :string, limit: 20)
 
-    assert command == Ecto.Migration.Dsl.ChangeTable[name: :products,
-                                                     changes: [{:change, Ecto.Migration.Dsl.Column[name: :summary,
-                                                                                                  type: :string,
-                                                                                                  limit: 20]}]]
+    assert command == ChangeTable[name: :products, changes: [
+        {:change, Column[name: :summary, type: :string, limit: 20]}]]
   end
 end
