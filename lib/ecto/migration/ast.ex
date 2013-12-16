57fc79bb74a72b8edde2a4eaf19ba0505b1d856d
Josh Nussbaum <joshnuss@gmail.com>
2013-12-16 18:59:14 -0500

    Removed uneeded records

--- lib/ecto/migration/ast.ex
+++ lib/ecto/migration/ast.ex
@@ -1,45 +1,4 @@
 defmodule Ecto.Migration.Ast do
-  defrecord Column, name: nil, type: nil, null: nil, limit: nil, default: :undefined
-
-  defrecord CreateTable, name: nil, columns: [] do
-    use Ecto.Migration.Dsl.ColumnAliases
-
-    def build(options // [key: true]) do
-      new.setup_key(options[:key])
-    end
-
-    def column(name, type, options // [], table) do
-      col = Column.new(Dict.merge(options, name: name, type: type))
-      table.columns(table.columns ++ [col])
-    end
-
-    def setup_key(true, table), do: table.primary_key(:id)
-    def setup_key(_, table),    do: table
-  end
-
-  defrecord ChangeTable, name: nil, changes: [] do
-    use Ecto.Migration.Dsl.ColumnAliases
-
-    def column(name, type, options // [], table) do
-      col = Column.new(Dict.merge(options, name: name, type: type))
-      table.changes(table.changes ++ [{:add, col}])
-    end
-
-    def remove(name, table) do
-      col = Column.new(name: name)
-      table.changes(table.changes ++ [{:remove, col}])
-    end
-
-    def change(name, type, options // [], table) do
-      col = Column.new(Dict.merge(options, name: name, type: type))
-      table.changes(table.changes ++ [{:change, col}])
-    end
-  end
-
-  defrecord DropTable, name: nil
-
-  defrecord CreateIndex, name: nil, table_name: nil, unique: false, columns: []
-  defrecord DropIndex, name: nil, table_name: nil, columns: []
 
   defrecord Table, name: nil, key: true
 
--- lib/ecto/migration/dsl.ex
+++ lib/ecto/migration/dsl.ex
@@ -1,23 +1,4 @@
 defmodule Ecto.Migration.Dsl do
-  defmodule ColumnAliases do
-    defmacro __using__(_module) do
-      quote do
-        def string(name, table),      do: table.column(name, :string)
-        def integer(name, table),     do: table.column(name, :integer)
-        def float(name, table),       do: table.column(name, :float)
-        def boolean(name, table),     do: table.column(name, :boolean)
-        def binary(name, table),      do: table.column(name, :binary)
-        def list(name, table),        do: table.column(name, :list)
-        def datetime(name, table),    do: table.column(name, :datetime)
-        def primary_key(name, table), do: table.column(name, :primary_key)
-
-        def timestamps(table) do
-          table.datetime(:created_at)
-               .datetime(:updated_at)
-        end
-      end
-    end
-  end
 
   defmodule MigrationRunner do
     use GenServer.Behaviour
