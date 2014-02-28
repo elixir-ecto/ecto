39b1f40c7467846c232a2473db75926ee9219a24
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2014-02-28 23:47:30 +0100

    Fix race condition in storage test

--- integration_test/pg/storage_test.exs
+++ integration_test/pg/storage_test.exs
@@ -3,53 +3,50 @@ defmodule Ecto.Integration.StorageTest do
 
   alias Ecto.Adapters.Postgres
 
-  def correct_params do 
+  def correct_params do
     [database: "storage_mgt",
      username: "postgres",
      password: "postgres",
      hostname: "localhost"]
   end
 
-  def wrong_user do 
+  def wrong_user do
     [database: "storage_mgt",
      username: "randomuser",
      password: "password1234",
      hostname: "localhost"]
   end
 
-  def drop_database do 
+  def drop_database do
     System.cmd ~s(psql -U postgres -c "DROP DATABASE IF EXISTS storage_mgt;")
   end
 
-  def create_database do 
+  def create_database do
     System.cmd ~s(psql -U postgres -c "CREATE DATABASE storage_mgt;")
+  end
+
+  teardown do
+    drop_database
   end
 
   test "storage up (twice in a row)" do
     assert Postgres.storage_up(correct_params) == :ok
-
     assert Postgres.storage_up(correct_params) == { :error, :already_up }
-
-    create_database
   end
 
   test "storage up (wrong credentials)" do
-    refute Postgres.storage_up(wrong_user) == :ok  
+    refute Postgres.storage_up(wrong_user) == :ok
   end
 
-  test "storage down (twice in a row)" do 
+  test "storage down (twice in a row)" do
     create_database
 
-    assert Postgres.storage_down(correct_params) == :ok 
-  
+    assert Postgres.storage_down(correct_params) == :ok
     assert Postgres.storage_down(correct_params) == { :error, :already_down }
-  end 
+  end
 
-  test "storage down (wrong credentials)" do 
+  test "storage down (wrong credentials)" do
     create_database
-
-    refute Postgres.storage_down(wrong_user) == :ok 
-
-    drop_database
+    refute Postgres.storage_down(wrong_user) == :ok
   end
 end
