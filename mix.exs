d53c3950cffb2ced075e6d26c5c7a1dc6b9ea5e8
José Valim <jose.valim@plataformatec.com.br>
2014-04-28 10:48:50 +0200

    Use decimal and postgrex packages

--- mix.exs
+++ mix.exs
@@ -2,28 +2,28 @@ defmodule Ecto.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :ecto,
-      version: "0.1.0-dev",
-      elixir: "~> 0.13.1-dev",
-      deps: deps,
-      build_per_environment: false,
-      test_paths: test_paths(Mix.env),
+    [app: :ecto,
+     version: "0.1.0-dev",
+     elixir: "~> 0.13.1-dev",
+     deps: deps,
+     build_per_environment: false,
+     test_paths: test_paths(Mix.env),
 
-      # Docs
-      name: "Ecto",
-      docs: &docs/0,
-      source_url: "https://github.com/elixir-lang/ecto" ]
+     # Docs
+     name: "Ecto",
+     docs: &docs/0,
+     source_url: "https://github.com/elixir-lang/ecto"]
   end
 
   def application do
-    [ applications: [:poolboy] ]
+    [applications: [:decimal, :poolboy]]
   end
 
   defp deps() do
-    [ { :poolboy, "~> 1.1.0", github: "devinus/poolboy" },
-      { :decimal, "~> 0.1.2", github: "ericmj/decimal", override: true },
-      { :postgrex, "~> 0.4.2", github: "ericmj/postgrex", optional: true },
-      { :ex_doc, github: "elixir-lang/ex_doc", only: :dev } ]
+    [{:poolboy,  "~> 1.2.0", github: "devinus/poolboy"},
+     {:decimal,  "~> 0.1.2"},
+     {:postgrex, "~> 0.4.2", optional: true},
+     {:ex_doc, github: "elixir-lang/ex_doc", only: :docs}]
   end
 
   defp test_paths(:pg),  do: ["integration_test/pg"]
@@ -31,8 +31,8 @@ defmodule Ecto.Mixfile do
   defp test_paths(_),    do: ["test"]
 
   defp docs do
-    [ source_ref: System.cmd("git rev-parse --verify --quiet HEAD"),
-      main: "overview",
-      readme: true ]
+    [source_ref: System.cmd("git rev-parse --verify --quiet HEAD"),
+     main: "overview",
+     readme: true]
   end
 end
