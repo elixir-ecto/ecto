183f72bb29911b1ba299a48ba55b54843ebc64ff
Josh Nussbaum <joshnuss@gmail.com>
2014-12-24 05:32:14 -0500

    Moved Runner start up to `Migrator.run/4`

--- lib/mix/tasks/ecto.migrate.ex
+++ lib/mix/tasks/ecto.migrate.ex
@@ -48,7 +48,6 @@ defmodule Mix.Tasks.Ecto.Migrate do
       opts = Keyword.put(opts, :all, true)
     end
 
-    Ecto.Migration.Runner.start_link(repo)
     migrator.(repo, migrations_path(repo), :up, opts)
   end
 end
