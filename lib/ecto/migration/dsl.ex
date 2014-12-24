4abe8a046edb81e8222cb07f72b38ad5ed0992bc
Josh Nussbaum <joshnuss@gmail.com>
2014-12-24 03:45:49 -0500

    Added Migration.DSL

---
+++ lib/ecto/migration/dsl.ex
@@ -0,0 +1,173 @@
+defmodule Ecto.Migration.DSL do
+  @moduledoc """
+  Functions and macros for defining migration operations.
+  """
+  alias Ecto.Migration.Table
+  alias Ecto.Migration.Index
+  alias Ecto.Migration.Runner
+
+  @doc """
+  Creates a table.
+
+  ## Examples
+
+      create table(:posts) do
+        add :title, :string, default: "Untitled"
+        add :body,  :text
+
+        timestamps
+      end
+
+  """
+  defmacro create(object, do: block) do
+    commands = case block do
+      {:__block__, _location, ops} -> ops
+      _ -> [block]
+    end
+
+    quote(location: :keep) do
+      %Table{key: key} = unquote(object)
+      id = if key, do: [add(:id, :primary_key)], else: []
+      execute {:create, unquote(object), id ++ unquote(commands) |> List.flatten}
+    end
+  end
+
+  @doc """
+  Alters a table.
+
+  ## Examples
+
+      alter table(:posts) do
+        add :summary, :text
+        modify :title, :text
+        remove :views
+        rename :slug, :permalink
+      end
+
+  """
+  defmacro alter(object, do: block) do
+    commands = case block do
+      {:__block__, _location, ops} -> ops
+      _ -> [block]
+    end
+
+    quote(location: :keep) do
+      execute {:alter, unquote(object), unquote(commands) |> List.flatten}
+    end
+  end
+
+  def create(object) do
+    execute {:create, object}
+  end
+
+  @doc """
+  Drops a table or index.
+
+  ## Examples
+
+      drop index(:posts, [:name])
+      drop table(:posts)
+
+  """
+  def drop(object) do
+    execute {:drop, object}
+  end
+
+  def table(name, opts \\ []) do
+    %Table{name: name, key: Dict.get(opts, :key, true)}
+  end
+
+  def index(columns, opts=[on: table]) do
+    %Index{table: table, columns: columns, unique: opts[:unique]}
+  end
+
+  def index(table, columns, opts \\ []) do
+    %Index{table: table, columns: columns, unique: opts[:unique]}
+  end
+
+  @doc """
+  Executes arbitrary SQL.
+
+  ## Examples
+
+    execute "UPDATE posts SET published_at = NULL"
+
+  """
+  def execute(command) do
+    Runner.execute command
+  end
+
+  @doc """
+  Add a column when creating or altering a table.
+
+  ## Examples
+
+      create table(:posts) do
+        add :title, :string, default: "Untitled"
+      end
+
+      alter table(:posts) do
+        add :summary, :text
+      end
+
+  """
+  def add(column, type, opts \\ []) do
+    {:add, column, type, opts}
+  end
+
+  @doc """
+  Modify a column when altering a table.
+
+  ## Examples
+
+      alter table(:posts) do
+        modify :title, :text
+      end
+
+  """
+  def modify(column, type, opts \\ []) do
+    {:modify, column, type, opts}
+  end
+
+  @doc """
+  Remove a column when altering a table.
+
+  ## Examples
+
+      alter table(:posts) do
+        remove :title
+      end
+
+  """
+  def remove(column) do
+    {:remove, column}
+  end
+
+  @doc """
+  Rename a column when altering a table.
+
+  ## Examples
+
+      alter table(:posts) do
+        rename :name, :title
+      end
+
+  """
+  def rename(from, to) do
+    {:rename, from, to}
+  end
+
+  @doc """
+  Adds `created_at` and `updated_at` columns to a table.
+
+  ## Examples
+
+      create table(:posts) do
+        timestamps
+      end
+
+  """
+  def timestamps do
+    [add(:created_at, :datetime), add(:updated_at, :datetime)]
+  end
+end
---
+++ test/ecto/migration/dsl_test.exs
@@ -0,0 +1,75 @@
+defmodule Ecto.Migration.DSLTest do
+  use ExUnit.Case, async: false
+
+  alias Ecto.Migration.Table
+  alias Ecto.Migration.Index
+
+  import Ecto.Migration.DSL
+
+  defmodule MockRunner do
+    use GenServer
+
+    def start_link do
+      GenServer.start_link(__MODULE__, [], [name: Ecto.Migration.Runner])
+    end
+
+    def handle_call({:execute, command}, _from, state) do
+      {:reply, {:executed, command}, state}
+    end
+  end
+
+  setup do
+    {:ok, _} = MockRunner.start_link
+    :ok
+  end
+
+  test "executing" do
+    assert execute("a response") == {:executed, "a response"}
+  end
+
+  test "creating table" do
+    response = create table(:products, key: true) do
+      add :name, :string
+      timestamps
+    end
+
+    assert response == {:executed, {:create, %Table{name: :products, key: true},
+                        [{:add, :id, :primary_key, []},
+                         {:add, :name, :string, []},
+                         {:add, :created_at, :datetime, []},
+                         {:add, :updated_at, :datetime, []}]}}
+  end
+
+  test "dropping table" do
+    response = drop table(:products)
+
+    assert response == {:executed, {:drop, %Table{name: :products}}}
+  end
+
+  test "creating index" do
+    response = create index(:products, [:name], unique: true)
+
+    assert response == {:executed, {:create, %Index{table: :products, columns: [:name], unique: true}}}
+  end
+
+  test "dropping index" do
+    response = drop index([:name], on: :products)
+
+    assert response == {:executed, {:drop, %Index{table: :products, columns: [:name], unique: nil}}}
+  end
+
+  test "alter table" do
+    response = alter table(:products) do
+      add :name, :string, default: 'Untitled'
+      modify :price, :integer, default: 99
+      remove :summary
+      rename :name, :title
+    end
+
+    assert response == {:executed, {:alter, %Table{name: :products},
+                        [{:add, :name, :string, [default: 'Untitled']},
+                         {:modify, :price, :integer, [default: 99]},
+                         {:remove, :summary},
+                         {:rename, :name, :title}]}}
+  end
+end
