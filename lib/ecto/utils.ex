78f24fc6b22da0d3b31ed83c27190d73b4867be5
DainisL <dainis186@gmail.com>
2015-01-07 16:38:55 +0200

    add db options for Repo config

--- lib/ecto/utils.ex
+++ lib/ecto/utils.ex
@@ -1,13 +1,21 @@
 defmodule Ecto.Utils do
   @doc """
   Parses an Ecto URL of the following format:
-  `ecto://username:password@hostname:port/database?opts=123` where all options
+  `ecto://username:password@hostname:port/database?opts=123`, %{} where all options
   but the database is optional.
+
+  Second parameters is for database options:
+    %{template: ~s(template0),
+      encoding: ~s(UTF8),
+      lc_collate: ~s(en_US.UTF-8),
+      lc_ctype: ~s(en_US.UTF-8)
+    }
+
 
   If `username` is not specified, `$PGUSER` or `$USER` will be used. `password`
   defaults to `$PGPASS`. `hostname` defaults to `$PGHOST` or `localhost`.
   """
-  def parse_url(url) do
+  def parse_url(url, db_opt\\ %{}) do
     unless String.match? url, ~r/^[^:\/?#\s]+:\/\// do
       raise Ecto.InvalidURLError, url: url, message: "url should start with a scheme, host should start with //"
     end
@@ -32,7 +40,15 @@ defmodule Ecto.Utils do
              port:     info.port ]
 
     opts = Enum.reject(opts, fn {_k, v} -> is_nil(v) end)
-    opts ++ query
+    opts ++ query ++ db_opts_mearge(db_opt)
+  end
+
+  def db_opts_mearge(opt\\ %{}) do
+    %{template: ~s(template0),
+      encoding: ~s(UTF8),
+      lc_collate: ~s(en_US.UTF-8),
+      lc_ctype: ~s(en_US.UTF-8)
+    } |> Map.merge(opt) |> Map.to_list
   end
 
   defp atomize_keys(dict) do
--- test/ecto/utils_test.exs
+++ test/ecto/utils_test.exs
@@ -14,6 +14,34 @@ defmodule Ecto.UtilsTest do
     assert {:a, "b"} in url
   end
 
+  test "parse_url return list with db options" do
+    opt = parse_url("ecto://eric:hunter2@host:12345/mydb?size=10&a=b", %{lc_collate: "en_IE.UTF-8"})
+    assert {:encoding, "UTF8"} in opt
+    assert {:lc_collate, "en_IE.UTF-8"} in opt
+  end
+
+  test "db_opts_mearge" do
+    new_opt = %{lc_collate: "en_IE.UTF-8", lc_ctype: "en_IE.UTF-8"}
+    opt = db_opts_mearge(new_opt)
+
+    assert opt[:lc_collate] == new_opt[:lc_collate]
+    assert opt[:lc_ctype] == new_opt[:lc_ctype]
+    assert is_list opt
+  end
+
+  test "when db_opts_mearge is empty " do
+    default_val = %{template: ~s(template0),
+      encoding: ~s(UTF8),
+      lc_collate: ~s(en_US.UTF-8),
+      lc_ctype: ~s(en_US.UTF-8)
+    }
+
+    opt = db_opts_mearge
+    assert opt[:lc_collate] == default_val[:lc_collate]
+    assert opt[:lc_ctype] == default_val[:lc_ctype]
+    assert is_list opt
+  end
+
   test "parse_urls empty username/password" do
     url = parse_url("ecto://host:12345/mydb?size=10&a=b")
     assert !Dict.has_key?(url, :username)
