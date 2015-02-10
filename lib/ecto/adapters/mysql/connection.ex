caa1779c17a84efde2a0d041a3a9c02bd7f33b20
Lauro Caetano <laurocaetano1@gmail.com>
2015-02-10 17:24:48 -0200

    Initial support for select on MySQL

--- lib/ecto/adapters/mysql/connection.ex
+++ lib/ecto/adapters/mysql/connection.ex
@@ -52,7 +52,82 @@ if Code.ensure_loaded?(Mariaex.Connection) do
       "ROLLBACK TO SAVEPOINT " <> savepoint
     end
 
+    ## Query
+
+    def all(query) do
+      sources = create_names(query)
+
+      select  = select(query.select, query.distincts, sources)
+      from    = from(sources)
+      where   = where(query.wheres, sources)
+
+      assemble([select, from])
+    end
+
+    ## Query Generation
+
+    alias Ecto.Query.SelectExpr
+    alias Ecto.Query.QueryExpr
+    alias Ecto.Query.JoinExpr
+
+    defp select(%SelectExpr{fields: fields}, [], sources) do
+      "SELECT " <> Enum.map_join(fields, ", ", &expr(&1, sources))
+    end
+
+    defp select(%SelectExpr{fields: fields}, distincts, sources) do
+      exprs =
+        Enum.map_join(distincts, ", ", fn
+          %QueryExpr{expr: expr} ->
+            Enum.map_join(expr, ", ", &expr(&1, sources))
+        end)
+
+      "SELECT DISTINCT ON (" <> exprs <> ") " <>
+        Enum.map_join(fields, ", ", &expr(&1, sources))
+    end
+
+    defp from(sources) do
+      {table, name, _model} = elem(sources, 0)
+      "FROM #{table} AS #{name}"
+    end
+
+    def where(wheres, sources) do
+      boolean("WHERE", wheres, sources)
+    end
+
+    defp boolean(_name, [], _sources), do: nil
+    defp boolean(name, query_exprs, sources) do
+      name <> " " <>
+        Enum.map_join(query_exprs, " AND ", fn
+          %QueryExpr{expr: expr} ->
+            "(" <> expr(expr, sources) <> ")"
+        end)
+    end
+
+    defp expr({{:., _, [{:&, _, [idx]}, field]}, _, []}, sources) when is_atom(field) do
+      {_, name, _} = elem(sources, idx)
+      "#{name}.#{field}"
+    end
+
     ## DDL
+
+    defp create_names(query) do
+      sources = query.sources |> Tuple.to_list
+
+      Enum.reduce(sources, [], fn {table, model}, names ->
+        name = unique_name(names, String.first(table), 0)
+        [{table, name, model}|names]
+      end) |> Enum.reverse |> List.to_tuple
+    end
+
+    # Brute force find unique name
+    defp unique_name(names, name, counter) do
+      counted_name = name <> Integer.to_string(counter)
+      if Enum.any?(names, fn {_, n, _} -> n == counted_name end) do
+        unique_name(names, name, counter + 1)
+      else
+        counted_name
+      end
+    end
 
     alias Ecto.Migration.Table
     alias Ecto.Migration.Index
