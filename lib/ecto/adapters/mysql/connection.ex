938cf5aeebac89fc315f8874cf79ae870411276e
Lauro Caetano <laurocaetano1@gmail.com>
2015-02-10 14:59:45 -0200

    Create the basic structure for create a new table on MySQL

--- lib/ecto/adapters/mysql/connection.ex
+++ lib/ecto/adapters/mysql/connection.ex
@@ -26,6 +26,7 @@ if Code.ensure_loaded?(Mariaex.Connection) do
       case Mariaex.Connection.query(conn, sql, params, opts) do
         {:ok, %Mariaex.Result{} = result} -> {:ok, Map.from_struct(result)}
         {:error, %Mariaex.Error{}} =  err -> err
+        :ok -> {:ok, %{}}
       end
     end
 
@@ -54,6 +55,8 @@ if Code.ensure_loaded?(Mariaex.Connection) do
     ## DDL
 
     alias Ecto.Migration.Table
+    alias Ecto.Migration.Index
+    alias Ecto.Migration.Reference
 
     def ddl_exists(%Table{name: name}) do
       """
@@ -64,10 +67,78 @@ if Code.ensure_loaded?(Mariaex.Connection) do
       """
     end
 
+    def execute_ddl({:create, %Table{} = table, columns}) do
+      "CREATE TABLE #{table.name} (#{column_definitions(columns)})"
+    end
+
+    defp column_definitions(columns) do
+      Enum.map_join(columns, ", ", &column_definition/1)
+    end
+
+    defp column_definition({:add, name, type, opts}) do
+      assemble([name, column_type(type, opts), column_options(name, opts)])
+    end
+
+    defp column_options(name, opts) do
+      default = Keyword.get(opts, :default)
+      null    = Keyword.get(opts, :null)
+      pk      = Keyword.get(opts, :primary_key)
+
+      [default_expr(default), null_expr(null), pk_expr(pk, name)]
+    end
+
+    defp pk_expr(true, name), do: ", PRIMARY KEY(#{name})"
+    defp pk_expr(_, _), do: nil
+
+    defp null_expr(false), do: "NOT NULL"
+    defp null_expr(true), do: "NULL"
+    defp null_expr(_), do: nil
+
+    defp default_expr(nil),
+      do: nil
+    defp default_expr(literal) when is_binary(literal),
+      do: "DEFAULT '#{escape_string(literal)}'"
+    defp default_expr(literal) when is_number(literal) or is_boolean(literal),
+      do: "DEFAULT #{literal}" # TODO: Check the boolean here :P
+    defp default_expr({:fragment, expr}),
+      do: "DEFAULT #{expr}"
+
+    defp column_type(%Reference{} = ref, opts) do
+      "#{column_type(ref.type, opts)} REFERENCES #{quote_name(ref.table)}(#{quote_name(ref.column)})"
+    end
+
+    defp column_type(type, opts) do
+      size      = Keyword.get(opts, :size)
+      precision = Keyword.get(opts, :precision)
+      scale     = Keyword.get(opts, :scale)
+      type_name = ecto_to_db(type)
+
+      cond do
+        size            -> "#{type_name}(#{size})"
+        precision       -> "#{type_name}(#{precision},#{scale || 0})"
+        type == :string -> "#{type_name}(255)"
+        true            -> "#{type_name}"
+      end
+    end
+
     ## Helpers
+
+    defp quote_name(name), do: "\"#{name}\""
+
+    defp assemble(list) do
+      list
+      |> List.flatten
+      |> Enum.filter(&(&1 != nil))
+      |> Enum.join(" ")
+    end
 
     defp escape_string(value) when is_binary(value) do
       :binary.replace(value, "'", "''", [:global])
     end
+
+    defp ecto_to_db(:string),     do: "varchar"
+    defp ecto_to_db(:datetime),   do: "timestamp"
+    defp ecto_to_db(:binary),     do: "blob"
+    defp ecto_to_db(other),       do: Atom.to_string(other)
   end
 end
