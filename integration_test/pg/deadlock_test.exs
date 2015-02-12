9db3a25d440204504e8261c7c09069ce4fe77ec2
José Valim <jose.valim@plataformatec.com.br>
2015-02-12 20:53:33 +0100

    Increase timeout values

--- integration_test/pg/deadlock_test.exs
+++ integration_test/pg/deadlock_test.exs
@@ -4,6 +4,7 @@ defmodule Ecto.Integration.DeadlockTest do
   use ExUnit.Case, async: true
   require Logger
 
+  @timeout 500
   alias Ecto.Integration.PoolRepo
 
   test "deadlocks reset worker" do
@@ -18,8 +19,8 @@ defmodule Ecto.Integration.DeadlockTest do
     tx1_result = PoolRepo.transaction fn ->
       acquire_deadlock(tx2, [1, 2])
     end
-    tx2_result = Task.await(tx2_task)
 
+    tx2_result = Task.await(tx2_task)
     assert Enum.sort([tx1_result, tx2_result]) == [{:error, :deadlocked}, {:ok, :acquired}]
   end
 
@@ -30,13 +31,13 @@ defmodule Ecto.Integration.DeadlockTest do
       PoolRepo.transaction fn ->
         pg_advisory_xact_lock(1)
         send(parent, :acquired1)
-        assert_receive :continue
+        assert_receive :continue, @timeout
 
         {:error, :continue} = PoolRepo.transaction fn ->
           try do
             pg_advisory_xact_lock(2)
             send(parent, :acquired2)
-            assert_receive :continue
+            assert_receive :continue, @timeout
             Ecto.Adapters.SQL.query(PoolRepo, "INVALID SQL --> ABORTS TRANSACTION", [])
           rescue
             err in [Postgrex.Error] ->
@@ -45,26 +46,26 @@ defmodule Ecto.Integration.DeadlockTest do
               assert_tx_aborted
               PoolRepo.rollback(:continue)
           else
-            _ -> assert false # should catch syntax error
+            _ -> flunk "expected syntax error"
           end
         end
 
         send(parent, :rollbacked_to_savepoint)
-        assert_receive :continue
+        assert_receive :continue, @timeout
       end
     end
 
     PoolRepo.transaction fn ->
-      assert_receive :acquired1 # other_tx has acquired lock on 1
+      assert_receive :acquired1, @timeout # other_tx has acquired lock on 1
       refute pg_try_advisory_xact_lock(1) # we can't get lock on 1
       send(other_tx, :continue)
 
-      assert_receive :acquired2 # other_tx has acquired lock on 2 in a nested tx
+      assert_receive :acquired2, @timeout # other_tx has acquired lock on 2 in a nested tx
       refute pg_try_advisory_xact_lock(1) # we still can't get lock on 1
       refute pg_try_advisory_xact_lock(2) # we now can't get lock on 2
       send(other_tx, :continue)
 
-      assert_receive :rollbacked_to_savepoint # other tx has rolled back nested tx after it aborted
+      assert_receive :rollbacked_to_savepoint, @timeout # other tx has rolled back nested tx after it aborted
       refute pg_try_advisory_xact_lock(1) # we still can't get lock on 1
       assert pg_try_advisory_xact_lock(2) # but we can get lock on 2
       send(other_tx, :continue)
@@ -77,8 +78,8 @@ defmodule Ecto.Integration.DeadlockTest do
   defp acquire_deadlock(other_tx, [key1, key2] = _locks) do
     pg_advisory_xact_lock(key1)  # acquire first lock
     Logger.debug "#{inspect self()} acquired #{key1}"
-    send other_tx, :acquired1    # signal other_tx that we acquired lock on key1
-    assert_receive :acquired1    # wait for other_tx to signal us that it acquired lock on its key1
+    send other_tx, :acquired1            # signal other_tx that we acquired lock on key1
+    assert_receive :acquired1, @timeout  # wait for other_tx to signal us that it acquired lock on its key1
     Logger.debug "#{inspect self()} continuing"
 
     try do
@@ -92,7 +93,7 @@ defmodule Ecto.Integration.DeadlockTest do
         # At this time the transaction count is actually 0 not 1 because Postgres
         # has killed the tx but Ecto doesn't know/care.
         {aborted_worker, _} = Process.get({:ecto_transaction_pid, Process.whereis(PoolRepo.__pool__)})
-        %{transactions: 1} = :sys.get_state(aborted_worker)
+        %{transactions: 1}  = :sys.get_state(aborted_worker)
 
         assert_tx_aborted
 
@@ -113,7 +114,7 @@ defmodule Ecto.Integration.DeadlockTest do
         # current transaction is aborted, commands ignored until end of transaction block
         assert %Postgrex.Error{postgres: %{code: "25P02"}} = err
     else
-      _ -> assert false # tx should be aborted
+      _ -> flunk "transaction should be aborted"
     end
   end
 
@@ -127,5 +128,4 @@ defmodule Ecto.Integration.DeadlockTest do
       Ecto.Adapters.SQL.query(PoolRepo, "SELECT pg_try_advisory_xact_lock($1);", [key])
     result
   end
-
 end
