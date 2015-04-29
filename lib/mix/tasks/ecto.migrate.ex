6e43f9a8cd82dd296aa704ea5f786134f6565089
jordan0day <jordan.day@perceptivesoftware.com>
2015-04-29 10:42:36 -0500

    add doc for --quiet command line option for ecto.migrate task

--- lib/mix/tasks/ecto.migrate.ex
+++ lib/mix/tasks/ecto.migrate.ex
@@ -33,6 +33,7 @@ defmodule Mix.Tasks.Ecto.Migrate do
     * `--step` / `-n` - run n number of pending migrations
     * `--to` / `-v` - run all migrations up to and including version
     * `--no-start` - do not start applications
+    * `--quiet` - do no log output
   """
 
   @doc false
