6632248ff0b60c04fee1799112b7f5f68b197db0
Jason Goldberger <jlgoldb2@asu.edu>
2015-07-10 17:04:34 -0700

    Added cities and countries to simple example
    Signed-off-by: José Valim <jose.valim@plataformatec.com.br>

---
+++ examples/simple/priv/repo/migrations/20150115192936_create_tables.exs
@@ -0,0 +1,32 @@
+defmodule Repo.CreateTables do
+  use Ecto.Migration
+
+  def up do
+    create table(:weather) do
+      add :city_id, :integer
+      add :temp_lo, :integer
+      add :temp_hi, :integer
+      add :prcp,    :float
+      timestamps
+    end
+    create index(:weather, [:city_id])
+
+    create table(:cities) do
+      add :name, :string, size: 40, null: false
+      add :country_id, :integer
+    end
+    create index(:cities, [:country_id])
+
+    create table(:countries) do
+      add :name, :string, size: 40, null: false
+    end
+  end
+
+  def down do
+    drop index(:weather, [:city_id])
+    drop index(:cities,  [:country_id])
+    drop table(:weather)
+    drop table(:cities)
+    drop table(:countries)
+  end
+end
