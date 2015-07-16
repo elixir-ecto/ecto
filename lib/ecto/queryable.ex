89fedfd3d8f67eaedb70f4371e65f15b613249c9
Gabriel Jaldon <gjaldon85@gmail.com>
2015-07-17 02:39:31 +0800

    Add search path to from queries

--- lib/ecto/queryable.ex
+++ lib/ecto/queryable.ex
@@ -15,13 +15,14 @@ end
 
 defimpl Ecto.Queryable, for: BitString do
   def to_query(source) when is_binary(source),
-    do: %Ecto.Query{from: {source, nil}}
+    do: %Ecto.Query{from: {source, source, nil}}
 end
 
 defimpl Ecto.Queryable, for: Atom do
   def to_query(module) do
     try do
-      %Ecto.Query{from: {module.__schema__(:source), module}}
+      source = module.__schema__(:source)
+      %Ecto.Query{from: {source, source, module}}
     rescue
       UndefinedFunctionError ->
         message = if :code.is_loaded(module) do
@@ -39,6 +40,7 @@ defimpl Ecto.Queryable, for: Atom do
 end
 
 defimpl Ecto.Queryable, for: Tuple do
-  def to_query(from = {source, model}) when is_binary(source) and is_atom(model),
+  def to_query(from = {search_path, source, model})
+    when is_binary(search_path) and is_binary(source) and is_atom(model),
     do: %Ecto.Query{from: from}
 end
