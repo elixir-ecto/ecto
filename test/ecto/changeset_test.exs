765ef2c31f09774ad9d49b5178bdfabefd70f8f8
Jan Pieper <kontakt@jan-pieper.info>
2015-08-30 12:11:20 +0200

    Allow lists for validate_length/3

--- test/ecto/changeset_test.exs
+++ test/ecto/changeset_test.exs
@@ -628,7 +628,7 @@ defmodule Ecto.ChangesetTest do
     assert changeset.errors == [title: "yada"]
   end
 
-  test "validate_length/3" do
+  test "validate_length/3 with string" do
     changeset = changeset(%{"title" => "world"}) |> validate_length(:title, min: 3, max: 7)
     assert changeset.valid?
     assert changeset.errors == []
@@ -654,6 +654,34 @@ defmodule Ecto.ChangesetTest do
 
     changeset = changeset(%{"title" => "world"}) |> validate_length(:title, is: 10, message: "yada")
     assert changeset.errors == [title: {"yada", count: 10}]
+  end
+
+  test "validate_length/3 with list" do
+    changeset = changeset(%{"topics" => ["Politics", "Security", "Economy", "Elections"]}) |> validate_length(:topics, min: 3, max: 7)
+    assert changeset.valid?
+    assert changeset.errors == []
+    assert changeset.validations == [topics: {:length, [min: 3, max: 7]}]
+
+    changeset = changeset(%{"topics" => ["Politics", "Security"]}) |> validate_length(:topics, min: 2, max: 2)
+    assert changeset.valid?
+
+    changeset = changeset(%{"topics" => ["Politics", "Security", "Economy"]}) |> validate_length(:topics, is: 3)
+    assert changeset.valid?
+
+    changeset = changeset(%{"topics" => ["Politics", "Security"]}) |> validate_length(:topics, min: 6, foo: true)
+    refute changeset.valid?
+    assert changeset.errors == [topics: {"should have at least %{count} items", count: 6}]
+
+    changeset = changeset(%{"topics" => ["Politics", "Security", "Economy"]}) |> validate_length(:topics, max: 2)
+    refute changeset.valid?
+    assert changeset.errors == [topics: {"should have at most %{count} items", count: 2}]
+
+    changeset = changeset(%{"topics" => ["Politics", "Security"]}) |> validate_length(:topics, is: 10)
+    refute changeset.valid?
+    assert changeset.errors == [topics: {"should have %{count} items", count: 10}]
+
+    changeset = changeset(%{"topics" => ["Politics", "Security"]}) |> validate_length(:topics, is: 10, message: "yada")
+    assert changeset.errors == [topics: {"yada", count: 10}]
   end
 
   test "validate_number/3" do
