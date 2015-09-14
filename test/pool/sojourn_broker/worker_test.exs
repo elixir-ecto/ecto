72d43abd39d73c7895f3e62cc97fbbda3440c832
James Fish <james@fishcakez.com>
2015-09-14 15:34:06 +0100

    Test sbroker worker matching a client in conn_exit
    Signed-off-by: José Valim <jose.valim@plataformatec.com.br>

--- test/pool/sojourn_broker/worker_test.exs
+++ test/pool/sojourn_broker/worker_test.exs
@@ -56,6 +56,39 @@ defmodule Ecto.Pools.SojournBroker.WorkerTest do
     assert conn1 != conn2
   end
 
+  test "worker restarts connection after cancel fails", context do
+    pool = context[:pool]
+    {:ok, _} = TestPool.start_link([pool_name: pool, size: 1, lazy: false])
+
+    conn1 = TestPool.transaction(pool, @timeout,
+      fn(:opened, _ref, {Connection, conn}, _) ->
+        conn
+      end)
+
+    await_len_r(pool, 1)
+
+    {:links, [worker]} = Process.info(conn1, :links)
+    :ok = :sys.suspend(worker)
+    ref = Process.monitor(conn1)
+    Process.exit(conn1, :kill)
+    receive do: ({:DOWN, ^ref, _, _, _} -> :ok)
+
+    conn2 = TestPool.transaction(pool, @timeout,
+      fn(:opened, _ref, {Connection, conn}, _) ->
+        :sys.resume(worker)
+        conn
+      end)
+
+    assert conn1 == conn2
+
+    conn3 = TestPool.transaction(pool, @timeout,
+      fn(:opened, _ref, {Connection, conn}, _) ->
+        conn
+      end)
+
+    assert conn2 != conn3
+  end
+
   defp await_len_r(pool, len) do
     case :sbroker.len_r(pool, @timeout) do
       ^len -> :ok
