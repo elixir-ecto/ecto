399e370881d233c5ae0b9a0c57baa655c4a79dcd
Joakim Ekström <jocke@aevy.com>
2015-11-28 15:01:38 +0100

    Fixes small spelling mistake in moduledoc

--- lib/ecto/exceptions.ex
+++ lib/ecto/exceptions.ex
@@ -99,7 +99,7 @@ end
 defmodule Ecto.NoPrimaryKeyValueError do
   @moduledoc """
   Raised at runtime when an operation that requires a primary key is invoked
-  with a model missing value for it's primary key
+  with a model missing value for its primary key
   """
   defexception [:message, :struct]
 
