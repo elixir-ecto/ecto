6f7ea370dfd744a07cf9b526aa1e4e7755bf8988
José Valim <jose.valim@plataformatec.com.br>
2016-02-13 18:02:50 +0100

    Remove imperative assignment from join

--- lib/ecto/query/builder/join.ex
+++ lib/ecto/query/builder/join.ex
@@ -110,15 +110,17 @@ defmodule Ecto.Query.Builder.Join do
     join_params = Builder.escape_params(join_params)
 
     qual = validate_qual(qual)
-    count_setter = nil
     validate_bind(join_bind, binding)
 
-    if join_bind != :_ and !count_bind do
-      # If count_bind is not an integer, make it a variable.
-      # The variable is the getter/setter storage.
-      count_bind = quote(do: count_bind)
-      count_setter = quote(do: unquote(count_bind) = Builder.count_binds(query))
-    end
+    {count_bind, count_setter} =
+      if join_bind != :_ and !count_bind do
+        # If count_bind is not an integer, make it a variable.
+        # The variable is the getter/setter storage.
+        {quote(do: count_bind),
+         quote(do: count_bind = Builder.count_binds(query))}
+      else
+        {count_bind, nil}
+      end
 
     binding = binding ++ [{join_bind, count_bind}]
     join_on = escape_on(on || true, binding, env)
