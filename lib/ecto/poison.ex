4ba9707af0de04f0fa09fbea62717894f23fdf3f
José Valim <jose.valim@plataformatec.com.br>
2016-05-24 16:28:06 +0200

    Provide Poison association encoding

--- lib/ecto/poison.ex
+++ lib/ecto/poison.ex
@@ -6,4 +6,12 @@ if Code.ensure_loaded?(Poison) do
   defimpl Poison.Encoder, for: [Ecto.Date, Ecto.Time, Ecto.DateTime] do
     def encode(dt, _opts), do: <<?", @for.to_iso8601(dt)::binary, ?">>
   end
+
+  defimpl Poison.Encoder, for: Ecto.Association.NotLoaded do
+    def encode(%{__owner__: owner, __field__: field}, _) do
+      raise "cannot encode association #{inspect field} from #{inspect owner} to " <>
+            "JSON because the association was not loaded. Please make sure you have " <>
+            "preloaded the association or remove it from the data to be encoded"
+    end
+  end
 end
--- test/ecto/poison_test.exs
+++ test/ecto/poison_test.exs
@@ -1,6 +1,14 @@
 defmodule Ecto.PoisonTest do
   use ExUnit.Case, async: true
 
+  defmodule User do
+    use Ecto.Schema
+
+    embedded_schema do
+      has_many :comments, Ecto.Comment
+    end
+  end
+
   test "encodes datetimes" do
     time = %Ecto.Time{hour: 1, min: 2, sec: 3}
     assert Poison.encode!(time) == ~s("01:02:03")
@@ -15,5 +23,12 @@ defmodule Ecto.PoisonTest do
   test "encodes decimal" do
     decimal = Decimal.new("1.0")
     assert Poison.encode!(decimal) == ~s("1.0")
+  end
+
+  test "fails on association not loaded" do
+    assert_raise RuntimeError,
+                 ~r/cannot encode association :comments from Ecto.PoisonTest.User to JSON/, fn ->
+      Poison.encode!(%User{}.comments)
+    end
   end
 end
