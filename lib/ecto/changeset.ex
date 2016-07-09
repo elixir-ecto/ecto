23bd406513a9d731bd30ff5b2726c664a1adcf9d
koolhazcker <koolhazcker@gmail.com>
2016-07-09 22:42:43 +0300

    changed the paragraph about validations

--- lib/ecto/changeset.ex
+++ lib/ecto/changeset.ex
@@ -47,9 +47,10 @@ defmodule Ecto.Changeset do
         end
       end
 
-  In the `changeset/2` function above, we define two validations -
-  one for checking the e-mail format and another to check the age -
-  as well as a unique constraint in the email field.
+  In the `changeset/2` function above, we define three validations -
+  one after another they check that `name` and `email` fields are present in the 
+  changeset, the e-mail is of the specified format, and the age is between 18 
+  and 100 - as well as a unique constraint in the email field.
 
   Let's suppose the e-mail is given but the age is invalid.  The
   changeset would have the following errors:
