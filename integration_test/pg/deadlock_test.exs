bf2287ae782dee868d41df30f78b1f537d29c9b9
Imran Ismail <imran.codely@gmail.com>
2016-07-09 18:16:13 +0800

    Update postgres integration test

--- integration_test/pg/deadlock_test.exs
+++ integration_test/pg/deadlock_test.exs
@@ -63,7 +63,7 @@ defmodule Ecto.Integration.DeadlockTest do
 
   defp assert_tx_aborted do
     try do
-      Ecto.Adapters.SQL.query!(PoolRepo, "SELECT 1", []);
+      PoolRepo.query!("SELECT 1");
     rescue
       err in [Postgrex.Error] ->
         # current transaction is aborted, commands ignored until end of transaction block
@@ -75,6 +75,6 @@ defmodule Ecto.Integration.DeadlockTest do
 
   defp pg_advisory_xact_lock(key) do
     %{rows: [[:void]]} =
-      Ecto.Adapters.SQL.query!(PoolRepo, "SELECT pg_advisory_xact_lock($1);", [key])
+      PoolRepo.query!("SELECT pg_advisory_xact_lock($1);", [key])
   end
 end
