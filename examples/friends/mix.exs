b4e82f80f44c71516188e0ca30a032e651fc0927
José Valim <jose.valim@plataformatec.com.br>
2016-07-30 11:31:03 +0200

    Update elixir dependency in friends app

--- examples/friends/mix.exs
+++ examples/friends/mix.exs
@@ -4,7 +4,7 @@ defmodule Friends.Mixfile do
   def project do
     [app: :friends,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -28,14 +28,12 @@ defmodule Friends.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [
-      # Normally you would specify a version of Ecto, like this:
-      #
-      # {:ecto, "~> 2.0"}
-      #
-      # It is not done in this instance because we want to refer to the local Ecto.
-      {:ecto, path: "../.."},
-      {:postgrex, "0.11.2"}
-    ]
+    # Normally you would specify a version of Ecto, like this:
+    #
+    # {:ecto, "~> 2.0"}
+    #
+    # It is not done in this instance because we want to refer to the local Ecto.
+    [{:ecto, path: "../.."},
+     {:postgrex, "0.11.2"}]
   end
 end
