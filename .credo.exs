bc2fa873c7efb4125f0cc796c23d13f1f59df0ff
Lucas Mazza <lucasmazza@users.noreply.github.com>
2017-04-18 06:30:23 -0300

    Update `credo.exs` for Credo 0.7.x (#2034)

--- .credo.exs
+++ .credo.exs
@@ -35,7 +35,7 @@
       {Credo.Check.Readability.ModuleAttributeNames},
       {Credo.Check.Readability.ModuleDoc},
       {Credo.Check.Readability.ModuleNames},
-      {Credo.Check.Readability.NoParenthesesWhenZeroArity, false},
+      {Credo.Check.Readability.ParenthesesOnZeroArityDefs, false},
       {Credo.Check.Readability.ParenthesesInCondition},
       {Credo.Check.Readability.PredicateFunctionNames},
       {Credo.Check.Readability.SinglePipe},
