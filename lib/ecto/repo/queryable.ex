8e1b378a6f79b0f26495578e57f896d3361c808a
Tobias Casper <tobias.casper@gmail.com>
2017-07-12 19:29:33 +0200

    Add support for clauses with nil values in `Repo.get_by(!)/2` (#2125)

--- lib/ecto/repo/queryable.ex
+++ lib/ecto/repo/queryable.ex
@@ -341,7 +341,14 @@ defmodule Ecto.Repo.Queryable do
   end
 
   defp query_for_get_by(_repo, queryable, clauses) do
-    Query.where(queryable, [], ^Enum.to_list(clauses))
+    clauses
+    |> Enum.to_list
+    |> Enum.reduce(queryable, fn
+      ({key, nil}, query) ->
+        Query.where(query, [x], is_nil(field(x, ^key)))
+      ({key, value}, query) ->
+        Query.where(query, [x], field(x, ^key) == ^value)
+    end)
   end
 
   defp query_for_aggregate(queryable, aggregate, field) do
