b9dff5ff48d40c854d77ccc8d88d25b3c929ec80
Andrew Dryga <andrew@dryga.com>
2017-07-21 16:46:37 +0300

    Get rid from exception text match for query that returns raw sql

--- integration_test/sql/sql.exs
+++ integration_test/sql/sql.exs
@@ -55,7 +55,7 @@ defmodule Ecto.Integration.SQLTest do
 
     assert %CorruptedPk{a: "abc"} = TestRepo.update!(schema |> Ecto.Changeset.change, force: true)
 
-    assert_raise Ecto.MultipleResultsError, ~s/expected at most one result but got 10 in query:\n\n/ <> _, fn ->
+    assert_raise Ecto.MultipleResultsError, fn ->
       TestRepo.delete!(schema)
     end
   end
