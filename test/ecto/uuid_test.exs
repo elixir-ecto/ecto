20a1f03dba76b6f5bd7296815a3a8bdc4f6b8c76
Michał Muskała <michal@muskala.eu>
2017-08-01 12:06:21 +0200

    Relax validations on UUID
    Closes #2147

--- test/ecto/uuid_test.exs
+++ test/ecto/uuid_test.exs
@@ -4,16 +4,17 @@ defmodule Ecto.UUIDTest do
   @test_uuid "601d74e4-a8d3-4b6e-8365-eddb4c893327"
   @test_uuid_upper_case "601D74E4-A8D3-4B6E-8365-EDDB4C893327"
   @test_uuid_invalid_characters "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  @test_uuid_invalid_shape "this is not UUID"
+  @test_uuid_nonconforming "00000000-0000-0000-0000-000000000034"
   @test_uuid_binary <<0x60, 0x1D, 0x74, 0xE4, 0xA8, 0xD3, 0x4B, 0x6E,
                       0x83, 0x65, 0xED, 0xDB, 0x4C, 0x89, 0x33, 0x27>>
+  @test_uuid_nonconforming_binary <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 52>>
 
   test "cast" do
     assert Ecto.UUID.cast(@test_uuid) == {:ok, @test_uuid}
     assert Ecto.UUID.cast(@test_uuid_binary) == {:ok, @test_uuid}
     assert Ecto.UUID.cast(@test_uuid_upper_case) == {:ok, String.downcase(@test_uuid_upper_case)}
     assert Ecto.UUID.cast(@test_uuid_invalid_characters) == :error
-    assert Ecto.UUID.cast(@test_uuid_invalid_shape) == :error
+    assert Ecto.UUID.cast(@test_uuid_nonconforming) == {:ok, @test_uuid_nonconforming}
     assert Ecto.UUID.cast(nil) == :error
   end
 
@@ -26,6 +27,7 @@ defmodule Ecto.UUIDTest do
 
   test "load" do
     assert Ecto.UUID.load(@test_uuid_binary) == {:ok, @test_uuid}
+    assert Ecto.UUID.load(@test_uuid_nonconforming_binary) == {:ok, @test_uuid_nonconforming}
     assert Ecto.UUID.load("") == :error
     assert_raise RuntimeError, ~r"trying to load string UUID as Ecto.UUID:", fn ->
       Ecto.UUID.load(@test_uuid)
@@ -34,6 +36,7 @@ defmodule Ecto.UUIDTest do
 
   test "dump" do
     assert Ecto.UUID.dump(@test_uuid) == {:ok, @test_uuid_binary}
+    assert Ecto.UUID.dump(@test_uuid_nonconforming) == {:ok, @test_uuid_nonconforming_binary}
     assert Ecto.UUID.dump(@test_uuid_binary) == :error
   end
 
