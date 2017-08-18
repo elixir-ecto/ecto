fd45f45c0c1a2f99ca61415ad1216f643ce794d1
José Valim <jose.valim@plataformatec.com.br>
2017-08-18 15:59:23 +0200

    Cast decimals to float in MySQL
    For aggregations, MySQL defaults to decimal and it does not
    provide an alternative to cast to float. Therefore we need
    to load decimals when floats as expected when it comes to
    MySQL.

--- lib/ecto/adapters/mysql.ex
+++ lib/ecto/adapters/mysql.ex
@@ -116,6 +116,7 @@ defmodule Ecto.Adapters.MySQL do
   def loaders(:map, type),            do: [&json_decode/1, type]
   def loaders({:map, _}, type),       do: [&json_decode/1, type]
   def loaders(:boolean, type),        do: [&bool_decode/1, type]
+  def loaders(:float, type),          do: [&float_decode/1, type]
   def loaders(:binary_id, type),      do: [Ecto.UUID, type]
   def loaders({:embed, _} = type, _), do: [&json_decode/1, &Ecto.Adapters.SQL.load_embed(type, &1)]
   def loaders(_, type),               do: [type]
@@ -125,6 +126,9 @@ defmodule Ecto.Adapters.MySQL do
   defp bool_decode(0), do: {:ok, false}
   defp bool_decode(1), do: {:ok, true}
   defp bool_decode(x), do: {:ok, x}
+
+  defp float_decode(%Decimal{} = decimal), do: {:ok, Decimal.to_float(decimal)}
+  defp float_decode(x), do: {:ok, x}
 
   defp json_decode(x) when is_binary(x),
     do: {:ok, Application.get_env(:ecto, :json_library).decode!(x)}
