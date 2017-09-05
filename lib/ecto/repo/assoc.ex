7a7acaf7733d517fd253bcf2a6fa639a531a2ad6
José Valim <jose.valim@plataformatec.com.br>
2017-09-05 15:45:54 +0200

    Ensure joins associations work select map

--- lib/ecto/repo/assoc.ex
+++ lib/ecto/repo/assoc.ex
@@ -14,37 +14,34 @@ defmodule Ecto.Repo.Assoc do
   def query(rows, [], _sources), do: rows
 
   def query(rows, assocs, sources) do
-    # Pre-create rose tree of reflections and accumulator
-    # dicts in the same structure as the fields tree
-    refls = create_refls(0, assocs, sources)
-    accs  = create_accs(assocs)
-
-    # Replace the dict in the accumulator by a list
-    # We use it as a flag to store the substructs
-    accs = put_elem(accs, 1, [])
+    # Create rose tree of accumulator dicts in the same
+    # structure as the fields tree
+    accs = create_accs(0, assocs, sources, [])
 
     # Populate tree of dicts of associated entities from the result set
-    {_keys, rows, sub_dicts} = Enum.reduce(rows, accs, fn row, acc ->
+    {_keys, _cache, rows, sub_dicts} = Enum.reduce(rows, accs, fn row, acc ->
       merge(row, acc, 0) |> elem(0)
     end)
 
+    # Create the reflections that will be loaded into memory.
+    refls = create_refls(0, assocs, sub_dicts, sources)
+
     # Retrieve and load the assocs from cached dictionaries recursively
     for {item, sub_structs} <- Enum.reverse(rows) do
-      [load_assocs(item, sub_dicts, refls)|sub_structs]
+      [load_assocs(item, refls)|sub_structs]
     end
   end
 
-  defp merge([struct|sub_structs], {keys, dict, sub_dicts}, parent_key) do
+  defp merge([struct|sub_structs], {primary_keys, cache, dict, sub_dicts}, parent_key) do
     child_key =
       if struct do
-        struct
-        |> Ecto.primary_key!()
-        |> Enum.map(&elem(&1, 1))
+        for primary_key <- primary_keys do
+          case Map.get(struct, primary_key) do
+            nil -> raise Ecto.NoPrimaryKeyValueError, struct: struct
+            value -> value
+          end
+        end
       end
-
-    if child_key && nil in child_key do
-      raise Ecto.NoPrimaryKeyValueError, struct: struct
-    end
 
     # Traverse sub_structs adding one by one to the tree.
     # Note we need to traverse even if we don't have a child_key
@@ -54,10 +51,10 @@ defmodule Ecto.Repo.Assoc do
 
     # Now if we have a struct and its parent key, we store the current
     # data unless we have already processed it.
-    cache = {parent_key, child_key}
+    cache_key = {parent_key, child_key}
 
-    if struct && parent_key && not Map.get(keys, cache, false) do
-      keys = Map.put(keys, cache, true)
+    if struct && parent_key && not Map.get(cache, cache_key, false) do
+      cache = Map.put(cache, cache_key, true)
       item = {child_key, struct}
 
       # If we have a list, we are at the root,
@@ -69,42 +66,44 @@ defmodule Ecto.Repo.Assoc do
           Map.update(dict, parent_key, [item], &[item|&1])
         end
 
-      {{keys, dict, sub_dicts}, sub_structs}
+      {{primary_keys, cache, dict, sub_dicts}, sub_structs}
     else
-      {{keys, dict, sub_dicts}, sub_structs}
+      {{primary_keys, cache, dict, sub_dicts}, sub_structs}
     end
   end
 
-  defp load_assocs({child_key, struct}, sub_dicts, refls) do
-    Enum.reduce :lists.zip(sub_dicts, refls), struct, fn
-      {{_keys, dict, sub_dicts}, {refl, refls}}, acc ->
-        %{field: field, cardinality: cardinality} = refl
-        loaded =
-          dict
-          |> Map.get(child_key, [])
-          |> Enum.reverse()
-          |> Enum.map(&load_assocs(&1, sub_dicts, refls))
-          |> maybe_first(cardinality)
-        Map.put(acc, field, loaded)
+  defp load_assocs({child_key, struct}, refls) do
+    Enum.reduce refls, struct, fn {dict, refl, sub_refls}, acc ->
+      %{field: field, cardinality: cardinality} = refl
+      loaded =
+        dict
+        |> Map.get(child_key, [])
+        |> Enum.reverse()
+        |> Enum.map(&load_assocs(&1, sub_refls))
+        |> maybe_first(cardinality)
+      Map.put(acc, field, loaded)
     end
   end
 
   defp maybe_first(list, :one), do: List.first(list)
   defp maybe_first(list, _), do: list
 
-  defp create_refls(idx, fields, sources) do
-    Enum.map(fields, fn {field, {child_idx, child_fields}} ->
-      {_source, schema} = elem(sources, idx)
-      {schema.__schema__(:association, field),
-       create_refls(child_idx, child_fields, sources)}
+  defp create_refls(idx, fields, dicts, sources) do
+    {_source, schema} = elem(sources, idx)
+
+    Enum.map(:lists.zip(dicts, fields), fn
+      {{_primary_keys, _cache, dict, sub_dicts}, {field, {child_idx, child_fields}}} ->
+        sub_refls = create_refls(child_idx, child_fields, sub_dicts, sources)
+        {dict, schema.__schema__(:association, field), sub_refls}
     end)
   end
 
-  defp create_accs(fields) do
-    acc = Enum.map(fields, fn {_field, {_child_idx, child_fields}} ->
-      create_accs(child_fields)
+  defp create_accs(idx, fields, sources, initial_dict) do
+    acc = Enum.map(fields, fn {_field, {child_idx, child_fields}} ->
+      create_accs(child_idx, child_fields, sources, %{})
     end)
 
-    {%{}, %{}, acc}
+    {_source, schema} = elem(sources, idx)
+    {schema.__schema__(:primary_key), %{}, initial_dict, acc}
   end
 end
