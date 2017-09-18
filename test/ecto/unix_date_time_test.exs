f518867f1e045c0d89634916807f7bb9253a8af6
Igor <borodark@gmail.com>
2017-09-18 19:15:15 -0400

    Documentation

--- test/ecto/unix_date_time_test.exs
+++ test/ecto/unix_date_time_test.exs
@@ -1,6 +1,10 @@
 defmodule UnixDateTime do
   @moduledoc """
   An Ecto type for Date Time stored in DB as integer produced by unix `date +%s` command
+  The DB column declared as 'NOT NULL DEFAULT 0'
+  The use of 0 instead nil is lame because `date +%s` returns 0 for 1970-01-01 00:00:00
+  and negative values represent number of seconds prior to that date
+  Unfortunately some legacy databases can not be changed but we want to use Ecto with these systems. 
   """
   @behaviour Ecto.Type
 
@@ -26,21 +30,21 @@ defmodule UnixDateTime do
   def cast(_), do: :error
 
   @doc """
-  Converts a string representing a UUID into a binary.
+  Converts a DateTime into an integer
   """
   @spec dump(t | any) :: {:ok, integer} | :error
-  def dump(%DateTime{} = date), do: {:ok, DateTime.to_unix(date)}
   # Write 0 for no value
   def dump(nil), do: {:ok, 0}
+  # Write integer for any valid date
+  def dump(%DateTime{} = date), do: {:ok, DateTime.to_unix(date)}
 
   def dump(_), do: :error
 
   @doc """
-  Converts an Integer into DateTime.
+  Converts an integer: into DateTime.
   """
   @spec load(integer) :: {:ok, t} | :error
-  # If DB contains 0 this means Date has a default value
-  # and it's blank
+  # If DB contains 0 this means Date has a default value and it's blank
   def load(0), do: {:ok, nil}
 
   def load(value) when is_integer(value) do
