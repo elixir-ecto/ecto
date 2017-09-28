da78b79299fe368504bdd190da92da5a20c8fdbd
Christian Kruse <christian@kruse.cool>
2017-09-28 11:53:55 +0200

    Support for `ON DELETE RESTRICT` and `ON UPDATE RESTRICT` in migrations (#2248)

--- lib/ecto/adapters/postgres/connection.ex
+++ lib/ecto/adapters/postgres/connection.ex
@@ -858,10 +858,12 @@ if Code.ensure_loaded?(Postgrex) do
 
     defp reference_on_delete(:nilify_all), do: " ON DELETE SET NULL"
     defp reference_on_delete(:delete_all), do: " ON DELETE CASCADE"
+    defp reference_on_delete(:restrict), do: " ON DELETE RESTRICT"
     defp reference_on_delete(_), do: []
 
     defp reference_on_update(:nilify_all), do: " ON UPDATE SET NULL"
     defp reference_on_update(:update_all), do: " ON UPDATE CASCADE"
+    defp reference_on_update(:restrict), do: " ON UPDATE RESTRICT"
     defp reference_on_update(_), do: []
 
     ## Helpers
