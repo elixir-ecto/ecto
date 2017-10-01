39f23475983228585d463d4d2a90937fd9bc1407
José Valim <jose.valim@plataformatec.com.br>
2017-10-01 20:43:11 +0200

    Update to ExDoc 0.17.0 and use new groups

--- mix.exs
+++ mix.exs
@@ -22,15 +22,12 @@ defmodule Ecto.Mixfile do
      preferred_cli_env: ["test.all": :test],
 
      # Hex
-     description: description(),
+     description: "A database wrapper and language integrated query for Elixir",
      package: package(),
 
      # Docs
      name: "Ecto",
-     docs: [source_ref: "v#{@version}", main: "Ecto",
-            canonical: "http://hexdocs.pm/ecto", logo: "guides/images/e.png",
-            source_url: "https://github.com/elixir-ecto/ecto",
-            extras: ["guides/Getting Started.md", "guides/Associations.md"]]]
+     docs: docs()]
   end
 
   def application do
@@ -56,7 +53,7 @@ defmodule Ecto.Mixfile do
       {:poison, "~> 2.2 or ~> 3.0", optional: true},
 
       # Docs
-      {:ex_doc, "~> 0.16", only: :docs},
+      {:ex_doc, "~> 0.17", only: :docs},
       {:inch_ex, ">= 0.0.0", only: :docs}
     ]
   end
@@ -64,18 +61,14 @@ defmodule Ecto.Mixfile do
   defp test_paths(adapter) when adapter in @adapters, do: ["integration_test/#{adapter}"]
   defp test_paths(_), do: ["test/ecto", "test/mix"]
 
-  defp description do
-    """
-    A database wrapper and language integrated query for Elixir.
-    """
-  end
-
   defp package do
-    [maintainers: ["Eric Meadows-Jönsson", "José Valim", "James Fish", "Michał Muskała"],
-     licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/elixir-ecto/ecto"},
-     files: ~w(mix.exs README.md CHANGELOG.md lib) ++
-            ~w(integration_test/cases integration_test/sql integration_test/support)]
+    [
+      maintainers: ["Eric Meadows-Jönsson", "José Valim", "James Fish", "Michał Muskała"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/elixir-ecto/ecto"},
+      files: ~w(mix.exs README.md CHANGELOG.md lib) ++
+             ~w(integration_test/cases integration_test/sql integration_test/support)
+    ]
   end
 
   defp test_adapters(args) do
@@ -93,5 +86,78 @@ defmodule Ecto.Mixfile do
     if res > 0 do
       System.at_exit(fn _ -> exit({:shutdown, 1}) end)
     end
+  end
+
+  defp docs do
+    [
+      main: "Ecto",
+      source_ref: "v#{@version}",
+      canonical: "http://hexdocs.pm/ecto",
+      logo: "guides/images/e.png",
+      source_url: "https://github.com/elixir-ecto/ecto",
+      extras: [
+        "guides/Getting Started.md",
+        "guides/Associations.md"
+      ],
+      groups_for_modules: [
+        # Ecto,
+        # Ecto.Changeset,
+        # Ecto.Migration,
+        # Ecto.Migrator,
+        # Ecto.Multi,
+        # Ecto.Schema,
+        # Ecto.Schema.Metadata,
+
+        "Repo and Queries": [
+          Ecto.LogEntry,
+          Ecto.Repo,
+          Ecto.Query,
+          Ecto.Query.API,
+          Ecto.Queryable,
+          Ecto.SubQuery
+        ],
+
+        "Types": [
+          Ecto.DataType,
+          Ecto.Date,
+          Ecto.DateTime,
+          Ecto.Time,
+          Ecto.Type,
+          Ecto.UUID,
+        ],
+
+        "Adapters": [
+          Ecto.Adapters.MySQL,
+          Ecto.Adapters.Postgres,
+          Ecto.Adapters.SQL,
+          Ecto.Adapters.SQL.Connection,
+          Ecto.Adapters.SQL.Sandbox,
+        ],
+
+        "Adapter specification": [
+          Ecto.Adapter,
+          Ecto.Adapter.Migration,
+          Ecto.Adapter.Storage,
+          Ecto.Adapter.Structure,
+          Ecto.Adapter.Transaction,
+        ],
+
+        "Association structs": [
+          Ecto.Association.BelongsTo,
+          Ecto.Association.Has,
+          Ecto.Association.HasThrough,
+          Ecto.Association.ManyToMany,
+          Ecto.Association.NotLoaded,
+        ],
+
+        "Migration structs": [
+          Ecto.Migration.Command,
+          Ecto.Migration.Constraint,
+          Ecto.Migration.Index,
+          Ecto.Migration.Reference,
+          Ecto.Migration.Table,
+        ]
+      ]
+    ]
   end
 end
