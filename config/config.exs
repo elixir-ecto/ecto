099ee511beb64a42f8cdaf44e7067c4e4ab106c6
tmbb <tmbb@campus.ul.pt>
2017-10-13 20:09:42 +0100

    removed redundant config files

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,3 @@ use Mix.Config
 # Disable sasl reports enabled by sbroker
 config :sasl, :sasl_error_logger, :false
 
-import_config "#{Mix.env}.exs"
--- config/docs.exs
+++
@@ -1,6 +0,0 @@
-use Mix.Config
-
-
-# Configure ExDoc to use makeup for syntax highlighting
-config :ex_doc, :markdown_processor, Makedown.ExDoc
-
