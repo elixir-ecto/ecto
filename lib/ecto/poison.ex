295482fe5323614ae71078e93929657c7538bf71
José Valim <jose.valim@plataformatec.com.br>
2017-11-05 11:05:04 +0100

    Improve Ecto.Association.NotLoaded error

--- lib/ecto/poison.ex
+++ lib/ecto/poison.ex
@@ -5,9 +5,23 @@ if Code.ensure_loaded?(Poison) do
 
   defimpl Poison.Encoder, for: Ecto.Association.NotLoaded do
     def encode(%{__owner__: owner, __field__: field}, _) do
-      raise "cannot encode association #{inspect field} from #{inspect owner} to " <>
-            "JSON because the association was not loaded. Please make sure you have " <>
-            "preloaded the association or remove it from the data to be encoded"
+      raise """
+      cannot encode association #{inspect field} from #{inspect owner} to \
+      JSON because the association was not loaded.
+
+      You can either preload the association:
+
+          Repo.preload(#{inspect owner}, #{inspect field})
+
+      Or choose to not encode the association when converting the struct \
+      to JSON by explicitly listing the JSON fields in your schema:
+
+          defmodule #{inspect owner} do
+            # ...
+
+            @derive {Poison.Encoder, only: [:name, :title, ...]}
+            schema ... do
+      """
     end
   end
 end
