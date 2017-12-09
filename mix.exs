1a386508d5a5b94eb22e9e0e67526e28638fc5d7
Michał Muskała <michal@muskala.eu>
2017-12-09 14:45:15 +0100

    Adds initial structure for benchmarks
    - Adds initial setup
    - Adds ElixirBench config.yml
    - Adds benchmarks for Repo.insert!/1

--- mix.exs
+++ mix.exs
@@ -51,7 +51,11 @@ defmodule Ecto.Mixfile do
 
       # Docs
       {:ex_doc, "~> 0.17", only: :docs},
-      {:inch_ex, ">= 0.0.0", only: :docs}
+      {:inch_ex, ">= 0.0.0", only: :docs},
+
+      # Benchmarks
+      {:benchee, "~> 0.11.0"},
+      {:benchee_json, "~> 0.4.0"}
     ]
   end
 
