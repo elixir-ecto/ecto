19bf44ad856c019dd54727ad3d1f6e712cdc45e8
José Valim <jose.valim@plataformatec.com.br>
2018-03-20 13:11:45 +0100

    Merge lines

--- mix.exs
+++ mix.exs
@@ -67,9 +67,8 @@ defmodule Ecto.Mixfile do
       maintainers: ["Eric Meadows-Jönsson", "José Valim", "James Fish", "Michał Muskała"],
       licenses: ["Apache 2.0"],
       links: %{"GitHub" => "https://github.com/elixir-ecto/ecto"},
-      files: ~w(mix.exs README.md CHANGELOG.md lib) ++
-             ~w(integration_test/cases integration_test/sql integration_test/support) ++
-             ~w(.formatter.exs)
+      files: ~w(.formatter.exs mix.exs README.md CHANGELOG.md lib) ++
+             ~w(integration_test/cases integration_test/sql integration_test/support)
     ]
   end
 
