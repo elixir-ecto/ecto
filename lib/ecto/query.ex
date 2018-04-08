ce87525ab0d88746a231bb2a06019b585c8be00a
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2018-04-08 09:26:38 +0200

    Update Ecto.Query docs with description of named bindings (#2503)

--- lib/ecto/query.ex
+++ lib/ecto/query.ex
@@ -197,9 +197,29 @@ defmodule Ecto.Query do
 
   In other words, `...` will include all the binding between the
   first and the last, which may be no binding at all, one or many.
-  Using `...` can be handy from time to time but most of its uses
-  can be avoided by relying on the keyword query syntax when writing
-  queries.
+
+  Another option for flexibly building queries with joins are
+  named bindings. Coming back to the previous example, provided
+  we bind a join to a concrete name:
+
+      posts_with_comments =
+        from p in query,
+          join: c in Comment, as: :comment, where: c.post_id == p.id
+
+  We can refer to it by that name using a following form of
+  bindings list:
+
+      from [p, comment: c] in posts_with_comments, select: {p.title, c.body}
+
+  This approach lets us not worry about keeping track of the position
+  of the bindings when composing the query.
+
+  What's more, a name can be assigned to the first binding as well:
+
+      from p in Post, as: :post
+
+  Only atoms are accepted for binding names. Named binding references
+  are expected to be placed in the tail position of the bindings list.
 
   ### Bindingless operations
 
