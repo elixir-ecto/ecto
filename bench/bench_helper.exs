51df7b6796e94d7e12f0abd5080e4364554b6a81
Tallys Martins <tallysmartins@gmail.com>
2018-06-18 21:25:12 -0300

    Split benchmarks in micro and macro level
    Signed-off-by: Tallys Martins <tallysmartins@gmail.com>

--- bench/bench_helper.exs
+++ bench/bench_helper.exs
@@ -10,10 +10,13 @@ Application.put_env(
   "ecto://" <> (System.get_env("MYSQL_URL") || "root@localhost")
 )
 
+# Initialize 
 Code.require_file("support/setup.exs", __DIR__)
 
-Code.load_file("load_bench.exs", __DIR__)
-Code.load_file("to_sql_bench.exs", __DIR__)
+# Micro benchmarks 
+Code.load_file("benchmarks/micro/load_bench.exs", __DIR__)
+Code.load_file("benchmarks/micro/to_sql_bench.exs", __DIR__)
 
-# Needs postgresql and mysql up and running
-# Code.load_file("insert_bench.exs", __DIR__)
+# Macro benchmarks needs postgresql and mysql up and running
+# Code.load_file("benchmarks/macro/insert_bench.exs", __DIR__)
+# Code.load_file("benchmarks/macro/all_bench.exs", __DIR__)
