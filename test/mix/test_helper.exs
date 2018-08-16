1c64d17d33fbfae132c0630d4e2bd4198647c1e8
José Valim <jose.valim@plataformatec.com.br>
2018-08-16 12:24:09 +0200

    Reenable io shell when possible

--- test/mix/test_helper.exs
+++ test/mix/test_helper.exs
@@ -6,3 +6,7 @@ Logger.configure(level: :info)
 
 Code.require_file "../../integration_test/support/file_helpers.exs", __DIR__
 ExUnit.start()
+
+if function_exported?(ExUnit, :after_suite, 1) do
+  ExUnit.after_suite(fn _ -> Mix.shell(Mix.Shell.IO) end)
+end
