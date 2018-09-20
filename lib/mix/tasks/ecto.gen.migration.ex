f758f9877b1564099e3251287ca6e47b226ee74e
José Valim <jose.valim@plataformatec.com.br>
2018-09-20 20:13:09 +0200

    Pass repo correctly to ecto.migrate

--- lib/mix/tasks/ecto.gen.migration.ex
+++ lib/mix/tasks/ecto.gen.migration.ex
@@ -61,7 +61,7 @@ defmodule Mix.Tasks.Ecto.Gen.Migration do
           create_file file, migration_template(assigns)
 
           if open?(file) and Mix.shell.yes?("Do you want to run this migration?") do
-            Mix.Task.run "ecto.migrate", [repo]
+            Mix.Task.run "ecto.migrate", ["-r", inspect(repo)]
           end
 
           file
