1cd59d89f577207ef63abfe2fab5bfeff82ae802
Gabe Jackson <gj@mail.co.de>
2018-09-30 01:35:52 -0400

    Continue updating Ecto.Migration docs
    [skip ci]

--- lib/ecto/migration.ex
+++ lib/ecto/migration.ex
@@ -150,8 +150,8 @@ defmodule Ecto.Migration do
   ## Comments
 
   Migrations where you create or alter a table support specifying table
-  and column comments, the same can be done when creating constraints
-  and indexes. At the moment there is support only for PostgreSQL.
+  and column comments. The same can be done when creating constraints
+  and indexes. At the moment, only PostgreSQL supports this feature.
 
       def up do
         create index("posts", [:name], comment: "Index Comment")
@@ -164,7 +164,7 @@ defmodule Ecto.Migration do
 
   ## Repo configuration
 
-  The following migration configurations are available for a given repository:
+  The following migration configuration options are available for a given repository:
 
     * `:migration_source` - Version numbers of migrations will be saved in a
       table named `schema_migrations` by default. You can configure the name of
@@ -172,25 +172,25 @@ defmodule Ecto.Migration do
 
           config :app, App.Repo, migration_source: "my_migrations"
 
-    * `:migration_primary_key` - Ecto uses the `:id` column with type
-      `:bigserial` but you can configure it via:
+    * `:migration_primary_key` - By default, Ecto uses the `:id` column with type
+      `:bigserial`, but you can configure it via:
 
           config :app, App.Repo, migration_primary_key: [name: :uuid, type: :binary_id]
 
-    * `:migration_timestamps` - Ecto uses type `:naive_datetime` but you
-      can configure it via:
+    * `:migration_timestamps` - By default, Ecto uses the `:naive_datetime` type, but
+      you can configure it via:
 
           config :app, App.Repo, migration_timestamps: [type: :utc_datetime]
 
-    * `:migration_lock` - Ecto will lock the migration table to handle concurrent
-      migrators using `FOR UPDATE` by default but you can configure it via:
+    * `:migration_lock` - By default, Ecto will lock the migration table to handle
+      concurrent migrators using `FOR UPDATE`, but you can configure it via:
 
           config :app, App.Repo, migration_lock: nil
 
     * `:migration_default_prefix` - Ecto defaults to `nil` for the database prefix for
-      migrations but you can configure it via:
+      migrations, but you can configure it via:
 
-          config :app, App.Repo, migration_default_prefix: "kewl"
+          config :app, App.Repo, migration_default_prefix: "my_prefix"
 
   """
 
@@ -198,7 +198,7 @@ defmodule Ecto.Migration do
     @moduledoc """
     Used internally by adapters.
 
-    To define an index in a migration, see `Ecto.Migration.index/3`
+    To define an index in a migration, see `Ecto.Migration.index/3`.
     """
     defstruct table: nil,
               prefix: nil,
@@ -229,7 +229,7 @@ defmodule Ecto.Migration do
     @moduledoc """
     Used internally by adapters.
 
-    To define a table in a migration, see `Ecto.Migration.table/2`
+    To define a table in a migration, see `Ecto.Migration.table/2`.
     """
     defstruct name: nil, prefix: nil, comment: nil, primary_key: true, engine: nil, options: nil
     @type t :: %__MODULE__{name: String.t, prefix: atom | nil, comment: String.t | nil, primary_key: boolean,
@@ -240,7 +240,7 @@ defmodule Ecto.Migration do
     @moduledoc """
     Used internally by adapters.
 
-    To define a reference in a migration, see `Ecto.Migration.references/2`
+    To define a reference in a migration, see `Ecto.Migration.references/2`.
     """
     defstruct name: nil, table: nil, column: :id, type: :bigserial, on_delete: :nothing, on_update: :nothing
     @type t :: %__MODULE__{table: String.t, column: atom, type: atom, on_delete: atom, on_update: atom}
@@ -250,7 +250,7 @@ defmodule Ecto.Migration do
     @moduledoc """
     Used internally by adapters.
 
-    To define a constraint in a migration, see `Ecto.Migration.constraint/3`
+    To define a constraint in a migration, see `Ecto.Migration.constraint/3`.
     """
     defstruct name: nil, table: nil, check: nil, exclude: nil, prefix: nil, comment: nil
     @type t :: %__MODULE__{name: atom, table: String.t, prefix: atom | nil,
@@ -262,9 +262,9 @@ defmodule Ecto.Migration do
     Used internally by adapters.
 
     This represents the up and down legs of a reversible raw command
-    that is usually define with `Ecto.Migration.execute/1`.
+    that is usually defined with `Ecto.Migration.execute/1`.
 
-    To define a reversible command in a migration, see `Ecto.Migration.execute/2`
+    To define a reversible command in a migration, see `Ecto.Migration.execute/2`.
     """
     defstruct up: nil, down: nil
     @type t :: %__MODULE__{up: String.t, down: String.t}
@@ -292,8 +292,8 @@ defmodule Ecto.Migration do
   @doc """
   Creates a table.
 
-  By default, the table will also include a primary_key of name `:id`
-  and type `:bigserial`. Check `table/2` docs for more information.
+  By default, the table will also include an `:id` primary key field that
+  has a type of `:bigserial`. Check the `table/2` docs for more information.
 
   ## Examples
 
@@ -312,7 +312,7 @@ defmodule Ecto.Migration do
   @doc """
   Creates a table if it does not exist.
 
-  Works just like `create/2` but does not raise an error when table
+  Works just like `create/2` but does not raise an error when the table
   already exists.
   """
   defmacro create_if_not_exists(object, do: block) do
@@ -368,8 +368,9 @@ defmodule Ecto.Migration do
     * a table with only an `:id` field
     * a constraint
 
-  When reversing (in `change` running backward) indexes are only dropped if they
-  exist and no errors are raised. To enforce dropping an index use `drop/1`.
+  When reversing (in a `change/0` running backwards), indexes are only dropped
+  if they exist, and no errors are raised. To enforce dropping an index, use
+  `drop/1`.
 
   ## Examples
 
@@ -444,7 +445,7 @@ defmodule Ecto.Migration do
   @doc """
   Drops a table or index if it exists.
 
-  Does not raise an error if table or index does not exist.
+  Does not raise an error if the specified table or index does not exist.
 
   ## Examples
 
@@ -458,7 +459,7 @@ defmodule Ecto.Migration do
   end
 
   @doc """
-  Returns a table struct that can be given on create, alter, etc.
+  Returns a table struct that can be given to create, alter, etc.
 
   ## Examples
 
@@ -476,12 +477,13 @@ defmodule Ecto.Migration do
 
   ## Options
 
-    * `:primary_key` - when false, does not generate primary key on table creation
+    * `:primary_key` - when `false`, a primary key field is not generated on table
+      creation.
     * `:engine` - customizes the table storage for supported databases. For MySQL,
-      the default is InnoDB
-    * `:prefix` - the prefix for the table
-    * `:options` - provide custom options that will be appended after generated
-      statement, for example "WITH", "INHERITS" or "ON COMMIT" clauses
+      the default is InnoDB.
+    * `:prefix` - the prefix for the table.
+    * `:options` - provide custom options that will be appended after the generated
+      statement. For example, "WITH", "INHERITS", or "ON COMMIT" clauses.
 
   """
   def table(name, opts \\ [])
@@ -497,20 +499,20 @@ defmodule Ecto.Migration do
   @doc ~S"""
   Returns an index struct that can be used on `create`, `drop`, etc.
 
-  Expects the table name as first argument and the index fields as
-  second. The field can be an atom, representing a column, or a
-  string representing an expression that is sent as is to the database.
-
-  Indexes are non-unique by default.
+  Expects the table name as the first argument and the index field(s) as
+  the second. The fields can be atoms, representing columns, or strings,
+  representing expressions that are sent as-is to the database.
 
   ## Options
 
-    * `:name` - the name of the index. Defaults to "#{table}_#{column}_index"
-    * `:unique` - if the column(s) is unique or not
-    * `:concurrently` - if the index should be created/dropped concurrently
-    * `:using` - configures the index type
-    * `:prefix` - prefix for the index
-    * `:where` - the conditions for a partial index
+    * `:name` - the name of the index. Defaults to "#{table}_#{column}_index".
+    * `:unique` - indicates whether the index should be unique. Defaults to
+      `false`.
+    * `:concurrently` - indicates whether the index should be created/dropped
+      concurrently.
+    * `:using` - configures the index type (for PostgreSQL).
+    * `:prefix` - specify an optional prefix for the index.
+    * `:where` - specify conditions for a partial index.
 
   ## Adding/dropping indexes concurrently
 
@@ -525,32 +527,33 @@ defmodule Ecto.Migration do
 
   ## Index types
 
-  PostgreSQL supports several index types like B-tree, Hash or GiST. When
+  PostgreSQL supports several index types like B-tree, Hash, and GiST. When
   creating an index, the index type defaults to B-tree, but it can be specified
   with the `:using` option. The `:using` option can be an atom or a string; its
-  value is passed to the `USING` clause as is.
+  value is passed to the generated `USING` clause as is.
 
   More information on index types can be found in the [PostgreSQL
   docs](http://www.postgresql.org/docs/9.4/static/indexes-types.html).
 
   ## Partial indexes
 
-  Databases like PostgreSQL and MSSQL supports partial indexes.
+  Databases like PostgreSQL and MSSQL support partial indexes.
 
   A partial index is an index built over a subset of a table. The subset
   is defined by a conditional expression using the `:where` option.
   The `:where` option can be an atom or a string; its value is passed
-  to the `WHERE` clause as is.
+  to the generated `WHERE` clause as-is.
 
   More information on partial indexes can be found in the [PostgreSQL
   docs](http://www.postgresql.org/docs/9.4/static/indexes-partial.html).
 
   ## Examples
 
-      # Without a name, index defaults to products_category_id_sku_index
+      # With no name provided, the name of the below index defaults to
+      # products_category_id_sku_index
       create index("products", [:category_id, :sku], unique: true)
 
-      # Name can be given explicitly though
+      # The name can also be set explicitly
       drop index("products", [:category_id, :sku], name: :my_special_name)
 
       # Indexes can be added concurrently
@@ -559,16 +562,16 @@ defmodule Ecto.Migration do
       # The index type can be specified
       create index("products", [:name], using: :hash)
 
-      # Create a partial index
+      # Partial indexes are created by specifying a :where option
       create index("products", [:user_id], where: "price = 0", name: :free_products_index)
 
   Indexes also support custom expressions. Some databases may require the
-  index expression to be written between parens:
+  index expression to be written between parentheses:
 
-      # Create an index on custom expressions
+      # Create an index on a custom expression
       create index("products", ["(lower(name))"], name: :products_lower_name_index)
 
-      # To create a tsvector index with GIN on PostgreSQL
+      # Create a tsvector GIN index on PostgreSQL
       create index("products", ["(to_tsvector('english', name))"],
                    name: :products_name_vector, using: "GIN")
   """
@@ -630,7 +633,9 @@ defmodule Ecto.Migration do
 
   This is useful for database-specific functionality that does not
   warrant special support in Ecto, for example, creating and dropping
-  a PostgreSQL extension, and avoids having to define up/down blocks.
+  a PostgreSQL extension. The `execute/2` form avoids having to define
+  separate `up/0` and `down/0` blocks that each contain an `execute/1`
+  expression.
 
   ## Examples
 
