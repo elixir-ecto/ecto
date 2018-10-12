4a66b23ac214f366b9783c4cc330597102fb69f3
José Valim <jose.valim@plataformatec.com.br>
2018-10-12 12:02:09 +0200

    Move log tests to ecto_sql

--- integration_test/cases/repo.exs
+++ integration_test/cases/repo.exs
@@ -1277,36 +1277,6 @@ defmodule Ecto.Integration.RepoTest do
     assert [post3] == (from Post, where: ^params3) |> TestRepo.all
   end
 
-  ## Logging
-
-  test "log entry logged on query" do
-    log = fn latency, entry ->
-      assert %Ecto.LogEntry{result: {:ok, _}} = entry
-      assert latency == entry.query_time + entry.decode_time + entry.queue_time
-      send(self(), :logged)
-    end
-
-    Process.put(:telemetry, log)
-    _ = TestRepo.all(Post)
-    assert_received :logged
-  end
-
-  test "log entry with custom log level" do
-    assert ExUnit.CaptureLog.capture_log(fn ->
-             TestRepo.insert!(%Post{title: "1"}, [log: :error])
-           end) =~ "[error]"
-
-    # We cannot assert on the result because it depends on the suite log level
-    ExUnit.CaptureLog.capture_log(fn ->
-      TestRepo.insert!(%Post{title: "1"}, [log: true])
-    end)
-
-    # But this assertion is always true
-    assert ExUnit.CaptureLog.capture_log(fn ->
-      TestRepo.insert!(%Post{title: "1"}, [log: false])
-    end) == ""
-  end
-
   describe "upsert via insert" do
     @describetag :upsert
 
