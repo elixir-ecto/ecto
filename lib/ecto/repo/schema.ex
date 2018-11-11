968a5b9f54127d891df62baa3ee5dae2ea036cfa
Cẩm Huỳnh <huynhquancam@gmail.com>
2018-11-11 20:33:10 +0100

    Cover "replace" as well

--- lib/ecto/repo/schema.ex
+++ lib/ecto/repo/schema.ex
@@ -549,7 +549,8 @@ defmodule Ecto.Repo.Schema do
         raise ArgumentError, ":conflict_target option is required when :on_conflict is replace"
 
       {:replace, keys} when is_list(keys) ->
-        {keys, [], conflict_target}
+        fields = Enum.map(keys, &field_source!(schema, &1))
+        {fields, [], conflict_target}
 
       :replace_all_except_primary_key ->
         fields = replace_all_fields!(:replace_all_except_primary_key, schema)
@@ -573,8 +574,16 @@ defmodule Ecto.Repo.Schema do
   end
 
   defp replace_all_fields!(_kind, schema) do
-    for field <- schema.__schema__(:fields) do
-      schema.__schema__(:field_source, field)
+    Enum.map(schema.__schema__(:fields), &field_source!(schema, &1))
+  end
+
+  defp field_source!(schema, field) do
+    case schema.__schema__(:field_source, field) do
+      nil ->
+        raise ArgumentError, "unknown field for :on_conflict, got: #{inspect(field)}"
+
+      field ->
+        field
     end
   end
 
