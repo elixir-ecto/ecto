16120bb540277ee2522e133e757a1d286c1d8b84
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-12-14 13:54:35 -0700

    Add error clause result of rollback in multi trans
     - https://github.com/elixir-ecto/ecto/issues/2862

--- test/ecto/multi_test.exs
+++ test/ecto/multi_test.exs
@@ -336,6 +336,16 @@ defmodule Ecto.MultiTest do
       assert :ok == data.inside_ok
     end
 
+    test "rollbacks on errors in nested function" do
+      multi =
+        Ecto.Multi.new()
+        |> Ecto.Multi.run(:foo, fn repo, _ ->
+          repo.rollback(:bar)
+        end)
+
+      assert {:error, :bar} = TestRepo.transaction(multi)
+    end
+
     test "does not allow repeated operations" do
       fun = fn _, _ -> {:ok, :ok} end
 
