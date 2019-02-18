8c25abd2fcbff5c4d7f7eb9c8457e40c2c2c7df7
Cẩm Huỳnh <huynhquancam@gmail.com>
2019-02-18 07:17:46 +0100

    Raise error when preloading with assocs (#2916)

--- test/ecto/repo_test.exs
+++ test/ecto/repo_test.exs
@@ -270,6 +270,15 @@ defmodule Ecto.RepoTest do
       assert_raise Ecto.QueryError, ~r"preloads are not supported on streams", fn ->
         TestRepo.stream(query)
       end
+
+      query =
+        MySchemaWithAssoc
+        |> join(:inner, [m], p in assoc(m, :parent))
+        |> preload([_m, p], parent: p)
+
+      assert_raise Ecto.QueryError, ~r"preloads are not supported on streams", fn ->
+        TestRepo.stream(query)
+      end
     end
   end
 
