ae7406e9b8a9dd232d70f86451af53c88b13fcf4
José Valim <jose.valim@plataformatec.com.br>
2019-04-04 14:53:39 +0200

    Only compile Jason.Encoder if it is available, closes #2968

--- lib/ecto/json.ex
+++ lib/ecto/json.ex
@@ -1,40 +1,42 @@
-defimpl Jason.Encoder, for: Ecto.Association.NotLoaded do
-  def encode(%{__owner__: owner, __field__: field}, _) do
-    raise """
-    cannot encode association #{inspect(field)} from #{inspect(owner)} to \
-    JSON because the association was not loaded.
+if Code.ensure_loaded?(Jason.Encoder) do
+  defimpl Jason.Encoder, for: Ecto.Association.NotLoaded do
+    def encode(%{__owner__: owner, __field__: field}, _) do
+      raise """
+      cannot encode association #{inspect(field)} from #{inspect(owner)} to \
+      JSON because the association was not loaded.
 
-    You can either preload the association:
+      You can either preload the association:
 
-        Repo.preload(#{inspect(owner)}, #{inspect(field)})
+          Repo.preload(#{inspect(owner)}, #{inspect(field)})
 
-    Or choose to not encode the association when converting the struct \
-    to JSON by explicitly listing the JSON fields in your schema:
+      Or choose to not encode the association when converting the struct \
+      to JSON by explicitly listing the JSON fields in your schema:
 
-        defmodule #{inspect(owner)} do
-          # ...
+          defmodule #{inspect(owner)} do
+            # ...
 
-          @derive {Jason.Encoder, only: [:name, :title, ...]}
-          schema ... do
-    """
+            @derive {Jason.Encoder, only: [:name, :title, ...]}
+            schema ... do
+      """
+    end
   end
-end
 
-defimpl Jason.Encoder, for: Ecto.Schema.Metadata do
-  def encode(%{schema: schema}, _) do
-    raise """
-    cannot encode metadata from the :__meta__ field for #{inspect(schema)} \
-    to JSON. This metadata is used internally by ecto and should never be \
-    exposed externally.
+  defimpl Jason.Encoder, for: Ecto.Schema.Metadata do
+    def encode(%{schema: schema}, _) do
+      raise """
+      cannot encode metadata from the :__meta__ field for #{inspect(schema)} \
+      to JSON. This metadata is used internally by ecto and should never be \
+      exposed externally.
 
-    You can either map the schemas to remove the :__meta__ field before \
-    encoding to JSON, or explicit list the JSON fields in your schema:
+      You can either map the schemas to remove the :__meta__ field before \
+      encoding to JSON, or explicit list the JSON fields in your schema:
 
-        defmodule #{inspect(schema)} do
-          # ...
+          defmodule #{inspect(schema)} do
+            # ...
 
-          @derive {Jason.Encoder, only: [:name, :title, ...]}
-          schema ... do
-    """
+            @derive {Jason.Encoder, only: [:name, :title, ...]}
+            schema ... do
+      """
+    end
   end
 end
