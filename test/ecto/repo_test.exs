82bcd4c9bc4959b281a06542c05980ae76e5d5ce
Vasiliy Yakliushin <vyaklushin@gmail.com>
2019-06-30 11:11:30 +0200

    Add test for same query used twice with different prefixes

--- test/ecto/repo_test.exs
+++ test/ecto/repo_test.exs
@@ -243,6 +243,26 @@ defmodule Ecto.RepoTest do
       assert union_all_query.prefix == "public"
     end
 
+    test "allows to reuse query several times" do
+      shared_query = from(MySchema, union: ^from(MySchema), union_all: ^from(MySchema))
+
+      TestRepo.all(shared_query, prefix: "public")
+
+      assert_received {:all, query}
+      assert query.prefix == "public"
+      Enum.each(query.combinations, fn {_key, sub_query} ->
+        assert sub_query.prefix == "public"
+      end)
+
+      TestRepo.all(shared_query, prefix: "private")
+
+      assert_received {:all, query}
+      assert query.prefix == "private"
+      Enum.each(query.combinations, fn {_key, sub_query} ->
+        assert sub_query.prefix == "private"
+      end)
+    end
+
     test "removes any preload from query" do
       from(MySchemaWithAssoc, preload: :parent) |> TestRepo.aggregate(:count, :id)
       assert_received {:all, query}
