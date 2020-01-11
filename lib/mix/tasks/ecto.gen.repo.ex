9552839fe0bb854c4ee837cd79e2ceca8e65f02f
javier <4.1.4.1.done@gmail.com>
2020-01-11 10:32:40 -0800

    use config_path when writing new config in ecto.gen.repo. closes #3202

--- lib/mix/tasks/ecto.gen.repo.ex
+++ lib/mix/tasks/ecto.gen.repo.ex
@@ -54,10 +54,10 @@ defmodule Mix.Tasks.Ecto.Gen.Repo do
         config_first_line = get_first_config_line(check) <> "\n"
         new_contents = config_first_line <> "\n" <> config_template(opts)
         Mix.shell().info [:green, "* updating ", :reset, "config/config.exs"]
-        File.write! "config/config.exs", String.replace(contents, config_first_line, new_contents)
+        File.write! config_path, String.replace(contents, config_first_line, new_contents)
       {:error, _} ->
         config_first_line = Config |> Code.ensure_loaded?() |> get_first_config_line()
-        create_file "config/config.exs", config_first_line <> "\n\n" <> config_template(opts)
+        create_file config_path, config_first_line <> "\n\n" <> config_template(opts)
     end
 
     open?("config/config.exs")
