465c4f6c462cb3547a530d488cd203577c283726
Wojtek Mach <wojtek@wojtekmach.pl>
2020-01-21 17:58:34 +0100

    Ignore empty hostname when parsing database url
    By not putting the value into the list we give drivers a chance to use
    their own default hostname.

--- test/ecto/repo/supervisor_test.exs
+++ test/ecto/repo/supervisor_test.exs
@@ -37,6 +37,12 @@ defmodule Ecto.Repo.SupervisorTest do
             otp_app: :ecto, password: "hunter2", port: 12345, username: "eric"]
   end
 
+  test "ignores empty hostname" do
+    put_env(database: "hello", url: "ecto:///mydb")
+    {:ok, config} = runtime_config(:runtime, __MODULE__, :ecto, extra: "extra")
+    assert normalize(config) == [database: "mydb", extra: "extra", otp_app: :ecto]
+  end
+
   test "is no-op for nil or empty URL" do
     put_env(database: "hello", url: nil)
     {:ok, config} = runtime_config(:runtime, __MODULE__, :ecto, [])
