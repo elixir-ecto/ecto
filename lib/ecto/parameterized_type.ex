5ac7f0d8af094c564bb03a9e7746022c77a024fb
Mike Binns <TheFirstAvenger@users.noreply.github.com>
2020-08-12 11:27:33 -0400

    Add docs to callback and types in ParameterizedType (#3381)

--- lib/ecto/parameterized_type.ex
+++ lib/ecto/parameterized_type.ex
@@ -4,7 +4,7 @@ defmodule Ecto.ParameterizedType do
 
   Paramterized types allow a set of options to be specified in the schema
   which are initialized on compilation and passed to the callback functions
-  as the last argument. 
+  as the last argument.
 
   For example, `field :foo, :string` behaves the same for every field.
   On the other hand, `field :foo, Ecto.Enum, values: [:foo, :bar, :baz]`
@@ -19,7 +19,7 @@ defmodule Ecto.ParameterizedType do
   and more. For example, parameterized types can handle `nil` values
   in both `load` and `dump` callbacks, they can customize `cast` behavior
   per query and per changeset, and also control how values are embedded.
-  
+
   However, parameterized types are also more complex. Therefore, if
   everything you need to achieve can be done with basic types, they
   should be preferred to parameterized ones.
@@ -66,23 +66,82 @@ defmodule Ecto.ParameterizedType do
 
   """
 
+  @typedoc """
+  The keyword options passed from the Schema's field macro into `c:init/1`
+  """
   @type opts :: keyword()
 
+  @typedoc """
+  The parameters for the ParameterizedType
+
+  This is thet value passed back from `c:init/1` and subsequently passed as the last argument to all callbacks.
+  Idiomatically it is a map.
+  """
   @type params :: term()
 
+  @doc """
+  Callback to convert the options specified in  the field macro into parameters to be used in other callbacks.
+
+  This function is called at compile time, and should raise if invalid values are specified. It is idiomatic
+  that the parameters returned from this are a map. `field` and `schema` will be injected into the options
+  automatically
+
+  For example, this schema specification
+
+      schema "my_table" do
+        field :my_field, MyParameterizedType, opt1: :foo, opt2: nil
+      end
+
+  will result in the call:
+
+      MyParameterizedType.init([schema: "my_table", field: :my_field, opt1: :foo, opt2: nil])
+
+  """
   @callback init(opts :: opts()) :: params()
 
+  @doc """
+  Casts the given input to the ParameterizedType with the given parameters.
+
+  For more information on casting, see `c:Ecto.Type.cast/1`
+  """
   @callback cast(data :: term, params :: params()) ::
               {:ok, term} | {:error, keyword()} | :error
 
+  @doc """
+  Loads the given term into a ParameterizedType.
+
+  For more information on loading, see `c:Ecto.Type.load/1`
+
+  Note that this callback *will* be called when loading a `nil` value, unlike `c:Ecto.Type.load/1`.
+  """
   @callback load(value :: any(), loader :: function(), params :: params()) :: {:ok, value :: any()} | :error
 
+  @doc """
+  Dumps the given term into an Ecto native type.
+
+  For more information on dumping, see `c:Ecto.Type.dump/1`
+
+  Note that this callback *will* be called when dumping a `nil` value, unlike `c:Ecto.Type.dump/1`.
+  """
   @callback dump(value :: any(), dumper :: function(), params :: params()) :: {:ok, value :: any()} | :error
 
+  @doc """
+  Returns the underlying schema type for the ParameterizedType.
+
+  For more information on schema types, see `c:Ecto.Type.type/0`
+  """
   @callback type(params :: params()) :: Ecto.Type.t()
 
+  @doc """
+  Checks if two terms are semantically equal.
+  """
   @callback equal?(value1 :: any(), value2 :: any(), params :: params()) :: boolean()
 
+  @doc """
+  Dictates how the type should be treated inside embeds.
+
+  For more information on embedding, see `c:Ecto.Type.embed_as/1`
+  """
   @callback embed_as(format :: atom(), params :: params()) :: :self | :dump
 
   @doc false
