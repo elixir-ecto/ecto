e207578b9eb2fb42eaf0d2bc02158e8ea12a3b05
José Valim <jose.valim@dashbit.co>
2021-07-03 22:48:16 +0200

    Support latest telemetry

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Ecto.MixProject do
 
   defp deps do
     [
-      {:telemetry, "~> 0.4"},
+      {:telemetry, "~> 0.4 or ~> 1.0"},
       {:decimal, "~> 1.6 or ~> 2.0"},
       {:jason, "~> 1.0", optional: true},
       {:ex_doc, "~> 0.20", only: :docs}
