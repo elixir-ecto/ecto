e205d63d4a7d57fa6b9981bf6c2bdb58863134c5
Aziz Köksal <aziz.koeksal@gmail.com>
2021-07-09 18:29:39 +0200

    Allow json_extract_path and Access.get version in type macro.

--- lib/ecto/query/builder.ex
+++ lib/ecto/query/builder.ex
@@ -109,6 +109,14 @@ defmodule Ecto.Query.Builder do
     escape_with_type(expr, type, params_acc, vars, env)
   end
 
+  def escape({:type, _, [{:json_extract_path, _, [_ | _]} = expr, type]}, _type, params_acc, vars, env) do
+    escape_with_type(expr, type, params_acc, vars, env)
+  end
+
+  def escape({:type, _, [{{:., _, [Access, :get]}, _, _} = access_expr, type]}, _type, params_acc, vars, env) do
+    escape_with_type(access_expr, type, params_acc, vars, env)
+  end
+
   def escape({:type, meta, [expr, type]}, given_type, params_acc, vars, env) do
     case Macro.expand_once(expr, get_env(env)) do
       ^expr ->
