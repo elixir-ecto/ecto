711456ba21931e998a24a306526187695930249b
Nathan Long <him@nathanmlong.com>
2022-02-01 15:58:34 -0500

    Document ordering by an alias

--- lib/ecto/query.ex
+++ lib/ecto/query.ex
@@ -1483,6 +1483,24 @@ defmodule Ecto.Query do
         desc: c.id,
       ]
 
+  It's also possible to order by an aliased or calculated column:
+
+    from(c in City,
+      select: %{
+        name: c.name,
+        total_population:
+          fragment(
+            "COALESCE(?, ?) + ? AS total_population",
+            c.animal_population,
+            0,
+            c.human_population
+          )
+      },
+      order_by: [
+        {:desc, fragment("total_population")}
+      ]
+    )
+
   ## Expressions examples
 
       City |> order_by([c], asc: c.name, desc: c.population)
