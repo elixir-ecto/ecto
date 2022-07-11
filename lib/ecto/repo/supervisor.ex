15d06316fd572df987ce798951d38dbc04dbd4bd
Andrew Dryga <andrew@dryga.com>
2022-07-11 08:51:09 -0700

    Add support for settings idle_interval in connection URL

--- lib/ecto/repo/supervisor.ex
+++ lib/ecto/repo/supervisor.ex
@@ -3,7 +3,7 @@ defmodule Ecto.Repo.Supervisor do
   use Supervisor
 
   @defaults [timeout: 15000, pool_size: 10]
-  @integer_url_query_params ["timeout", "pool_size"]
+  @integer_url_query_params ["timeout", "pool_size", "idle_interval"]
 
   @doc """
   Starts the repo supervisor.
@@ -35,7 +35,7 @@ defmodule Ecto.Repo.Supervisor do
   defp telemetry_prefix(repo) do
     repo
     |> Module.split()
-    |> Enum.map(& &1 |> Macro.underscore() |> String.to_atom())
+    |> Enum.map(&(&1 |> Macro.underscore() |> String.to_atom()))
   end
 
   defp repo_init(type, repo, config) do
@@ -58,8 +58,9 @@ defmodule Ecto.Repo.Supervisor do
     end
 
     if Code.ensure_compiled(adapter) != {:module, adapter} do
-      raise ArgumentError, "adapter #{inspect adapter} was not compiled, " <>
-                           "ensure it is correct and it is included as a project dependency"
+      raise ArgumentError,
+            "adapter #{inspect(adapter)} was not compiled, " <>
+              "ensure it is correct and it is included as a project dependency"
     end
 
     behaviours =
@@ -124,6 +125,7 @@ defmodule Ecto.Repo.Supervisor do
 
   defp parse_uri_query(%URI{query: nil}),
     do: []
+
   defp parse_uri_query(%URI{query: query} = url) do
     query
     |> URI.query_decoder()
@@ -149,8 +151,8 @@ defmodule Ecto.Repo.Supervisor do
 
       _ ->
         raise Ecto.InvalidURLError,
-              url: url,
-              message: "can not parse value `#{value}` for parameter `#{key}` as an integer"
+          url: url,
+          message: "can not parse value `#{value}` for parameter `#{key}` as an integer"
     end
   end
 
