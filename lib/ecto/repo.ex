ccd3ec4a7ece0dc2556ee74dc0d5032ee87e9d19
Dave Lucia <davelucianyc@gmail.com>
2022-08-24 20:16:27 -0400

    Update lib/ecto/repo.ex
    Co-authored-by: felipe stival <14948182+v0idpwn@users.noreply.github.com>

--- lib/ecto/repo.ex
+++ lib/ecto/repo.ex
@@ -1158,7 +1158,7 @@ defmodule Ecto.Repo do
       from(p in Post, where: p.id < 10, update: [set: [title: fragment("upper(?)", ^new_title)]])
       |> MyRepo.update_all([])
 
-      from(p in Post, where: p.id < 10, update: [set: [view_count: p.view_count * 1000]])
+      from(p in Post, where: p.id < 10, update: [set: [visits: p.visits * 1000]])
       |> MyRepo.update_all([])
 
   """
