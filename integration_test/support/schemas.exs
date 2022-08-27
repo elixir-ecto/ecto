abc2fb97e3f1b6cd42293e241129c2a1f39621a4
João Otávio Biondo <joao.otb@gmail.com>
2022-08-27 15:50:18 +0200

    Add integration test for field sources with json_extract_path

--- integration_test/support/schemas.exs
+++ integration_test/support/schemas.exs
@@ -264,12 +264,13 @@ defmodule Ecto.Integration.Order do
 
     * Text columns
     * Embedding one schema
+    * Field source with json_extract_path
 
   """
   use Ecto.Integration.Schema
 
   schema "orders" do
-    field :meta, :map
+    field :metadata, :map, source: :meta
     embeds_one :item, Ecto.Integration.Item
     embeds_many :items, Ecto.Integration.Item
     belongs_to :permalink, Ecto.Integration.Permalink
