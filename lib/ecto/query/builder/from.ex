1687775ae45e8fbed2dd5690eecff777a2515601
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-10-10 22:46:07 +0700

    remove dynamic_hint type

--- lib/ecto/query/builder/from.ex
+++ lib/ecto/query/builder/from.ex
@@ -51,7 +51,7 @@ defmodule Ecto.Query.Builder.From do
 
   defp escape_source(query, _env), do: query
   
-  @typep hints :: [String.t() | dynamic_hint]
+  @typep hints :: [String.t() | {atom, term}]
 
   @doc """
   Builds a quoted expression.
