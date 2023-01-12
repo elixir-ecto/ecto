c76c8bb30c6f0fe56df4d27a31830548dcc6bf3a
Chris Bailey <me@cbailey.co.uk>
2023-01-12 11:50:41 +0000

    Update implementation of `Ecto.Query.exclude/2` to allow exclusion of `update`s

--- lib/ecto/query.ex
+++ lib/ecto/query.ex
@@ -798,6 +798,7 @@ defmodule Ecto.Query do
       Ecto.Query.exclude(query, :offset)
       Ecto.Query.exclude(query, :lock)
       Ecto.Query.exclude(query, :preload)
+      Ecto.Query.exclude(query, :update)
 
   You can also remove specific joins as well such as `left_join` and
   `inner_join`:
@@ -839,6 +840,7 @@ defmodule Ecto.Query do
   defp do_exclude(%Ecto.Query{} = query, :offset), do: %{query | offset: nil}
   defp do_exclude(%Ecto.Query{} = query, :lock), do: %{query | lock: nil}
   defp do_exclude(%Ecto.Query{} = query, :preload), do: %{query | preloads: [], assocs: []}
+  defp do_exclude(%Ecto.Query{} = query, :update), do: %{query | updates: []}
 
   @doc """
   Creates a query.
