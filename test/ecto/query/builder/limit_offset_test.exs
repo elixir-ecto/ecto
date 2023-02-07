7bbb437c2bd1e56f10bbccad17b503a6d39b6ba4
Greg Rychlewski <greg.rychlewski@gmail.com>
2023-02-07 08:28:18 -0500

    don't check for query variables in limit/offset

--- test/ecto/query/builder/limit_offset_test.exs
+++ test/ecto/query/builder/limit_offset_test.exs
@@ -1,10 +1,7 @@
-Code.require_file "../../../support/eval_helpers.exs", __DIR__
-
 defmodule Ecto.Query.Builder.LimitOffsetTest do
   use ExUnit.Case, async: true
 
   import Ecto.Query
-  import Support.EvalHelpers
 
   test "overrides on duplicated limit and offset" do
     query = "posts" |> limit([], 1) |> limit([], 2)
@@ -12,15 +9,5 @@ defmodule Ecto.Query.Builder.LimitOffsetTest do
 
     query = "posts" |> offset([], 1) |> offset([], 2) |> select([], 3)
     assert query.offset.expr == 2
-  end
-
-  test "does not allow query variables in limit and offset" do
-    assert_raise Ecto.Query.CompileError, "query variables are not allowed in limit expression", fn ->
-      quote_and_eval from p in "posts", limit: p.x + 1
-    end
-
-    assert_raise Ecto.Query.CompileError, "query variables are not allowed in offset expression", fn ->
-      quote_and_eval from p in "posts", offset: p.x + 2
-    end
   end
 end
