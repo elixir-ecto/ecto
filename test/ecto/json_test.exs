64559c997cbbe6b3b2b49f0cfe4df6789f15d503
Tyler Witt <Ty.Mo.Witt@gmail.com>
2025-01-11 01:14:31 +0700

    DRY up json protocol usage

--- test/ecto/json_test.exs
+++ test/ecto/json_test.exs
@@ -3,7 +3,10 @@ defmodule Ecto.JsonTest do
 
   @implementations [{Jason, Jason.Encoder}, {JSON, JSON.Encoder}]
 
-  loaded_implementations = for {_lib, encoder} = implementation <- @implementations, Code.ensure_loaded?(encoder), do: implementation
+  loaded_implementations =
+    for {_lib, encoder} = implementation <- @implementations,
+        Code.ensure_loaded?(encoder),
+        do: implementation
 
   defmodule User do
     use Ecto.Schema
@@ -15,7 +18,6 @@ defmodule Ecto.JsonTest do
   end
 
   for {json_library, _encoder} <- loaded_implementations do
-
     describe to_string(json_library) do
       test "encodes decimal" do
         decimal = Decimal.new("1.0")
@@ -24,14 +26,14 @@ defmodule Ecto.JsonTest do
 
       test "fails on association not loaded" do
         assert_raise RuntimeError,
-                    ~r/cannot encode association :comments from Ecto.JsonTest.User to JSON/,
-                    fn -> unquote(json_library).encode!(%User{}.comments) end
+                     ~r/cannot encode association :comments from Ecto.JsonTest.User to JSON/,
+                     fn -> unquote(json_library).encode!(%User{}.comments) end
       end
 
       test "fails when encoding __meta__" do
         assert_raise RuntimeError,
-                    ~r/cannot encode metadata from the :__meta__ field for Ecto.JsonTest.User to JSON/,
-                    fn -> unquote(json_library).encode!(%User{comments: []}) end
+                     ~r/cannot encode metadata from the :__meta__ field for Ecto.JsonTest.User to JSON/,
+                     fn -> unquote(json_library).encode!(%User{comments: []}) end
       end
     end
   end
