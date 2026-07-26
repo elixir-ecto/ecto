bac6db2106ff51dd63be6637cabb90cdf321d95d
Łukasz Samson <lukaszsamson@gmail.com>
2026-07-26 15:11:42 +0200

    Preserve `prepare_changes` in `Ecto.Changeset.merge/2` (#4768)

--- lib/ecto/changeset.ex
+++ lib/ecto/changeset.ex
@@ -1549,6 +1549,8 @@ defmodule Ecto.Changeset do
     * `required` - required fields are merged; all the fields that appear
       in the required list of both changesets are moved to the required
       list of the resulting changeset.
+    * `prepare` - prepare callbacks are concatenated in the order of the
+      changesets being merged.
 
   ## Examples
 
@@ -1574,6 +1576,7 @@ defmodule Ecto.Changeset do
     new_filters = Map.merge(cs1.filters, cs2.filters)
     new_validations = cs1.validations ++ cs2.validations
     new_constraints = cs1.constraints ++ cs2.constraints
+    new_prepare = cs2.prepare ++ cs1.prepare
 
     # They are always set, so they should never be nil
     _ = merge_identical(cs1.empty_values, cs2.empty_values, "empty values")
@@ -1586,7 +1589,8 @@ defmodule Ecto.Changeset do
           filters: new_filters,
           action: new_action,
           validations: new_validations,
-          constraints: new_constraints
+          constraints: new_constraints,
+          prepare: new_prepare
       },
       cs2
     )
